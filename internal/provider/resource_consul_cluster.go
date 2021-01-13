8c4566138640e55aace78ac190fc028499c3a20e
Andrew Gray <andrew.gray@hashicorp.com>
2021-01-13 09:32:44 -0600

    Wire up initial HCP Consul cluster resource scaffolding

---
+++ internal/provider/resource_consul_cluster.go
@@ -0,0 +1,8 @@
+package provider
+
+import "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+// resourceConsulCluster represents an HCP Consul cluster
+func resourceConsulCluster() *schema.Resource {
+	return &schema.Resource{}
+}
