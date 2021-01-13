425e4b1a511ca266dbbed84e4da99f2a4a2cc4e7
Andrew Gray <andrew.gray@hashicorp.com>
2021-01-13 09:55:04 -0600

    Wire up description, timeouts, importer stub, and CRUD context stubs

--- internal/provider/resource_consul_cluster.go
+++ internal/provider/resource_consul_cluster.go
@@ -1,8 +1,61 @@
 package provider
 
-import "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+import (
+	"context"
+	"time"
 
-// resourceConsulCluster represents an HCP Consul cluster
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+// defaultClusterTimeoutDuration is the amount of time that can elapse
+// before a cluster read operation should timeout.
+var defaultClusterTimeoutDuration = time.Minute * 5
+
+// createUpdateTimeoutDuration is the amount of time that can elapse
+// before a cluster create or update operation should timeout.
+var createUpdateTimeoutDuration = time.Minute * 30
+
+// deleteTimeoutDuration is the amount of time that can elapse
+// before a cluster delete operation should timeout.
+var deleteTimeoutDuration = time.Minute * 25
+
+// resourceConsulCluster represents an HCP Consul cluster.
 func resourceConsulCluster() *schema.Resource {
-	return &schema.Resource{}
+	return &schema.Resource{
+		Description:   "The Consul cluster resource allow you to manage an HCP Consul cluster.",
+		CreateContext: resourceConsulClusterCreate,
+		ReadContext:   resourceConsulClusterRead,
+		UpdateContext: resourceConsulClusterUpdate,
+		DeleteContext: resourceConsulClusterDelete,
+		Timeouts: &schema.ResourceTimeout{
+			Default: &defaultClusterTimeoutDuration,
+			Create:  &createUpdateTimeoutDuration,
+			Update:  &createUpdateTimeoutDuration,
+			Delete:  &deleteTimeoutDuration,
+		},
+		Importer: &schema.ResourceImporter{
+			StateContext: resourceConsulClusterImport,
+		},
+	}
+}
+
+func resourceConsulClusterCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	return nil
+}
+
+func resourceConsulClusterRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	return nil
+}
+
+func resourceConsulClusterUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	return nil
+}
+
+func resourceConsulClusterDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	return nil
+}
+
+func resourceConsulClusterImport(ctx context.Context, d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
+	return nil, nil
 }
