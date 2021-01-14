38a623acef5fc860be10b447e3c38101736d20d0
Andrew Gray <andrew.gray@hashicorp.com>
2021-01-14 10:55:29 -0600

    resourceConsulClusterCreate: assert Consul version is valid

--- internal/consul/version.go
+++ internal/consul/version.go
@@ -1,6 +1,86 @@
 package consul
 
-import "strings"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+// HCPConsulAPIVersion is the version of the HCP Consul API we are using to retrieve versions.
+const HCPConsulAPIVersion = "2020-08-26"
+
+// Version represents a Consul version and the status of that version in regards to availability on HCP.
+type Version struct {
+	// Version is the Consul product version.
+	Version string `json:"version"`
+
+	// Status denotes the availability (and whether the version is recommended) of the associated Consul version.
+	Status string `json:"status"`
+}
+
+// availableVersionsResponse is the body of the HCP Consul versions response.
+type availableVersionsResponse struct {
+	// Versions is a slice of available Consul versions and their statuses.
+	Versions []Version `json:"versions"`
+}
+
+// GetAvailableHCPConsulVersions retrieves a slice of supported/available Consul versions from the HCP Consul API.
+func GetAvailableHCPConsulVersions(ctx context.Context, hcpApiDomain string) ([]Version, error) {
+	apiDomain := strings.TrimPrefix(hcpApiDomain, "https://")
+	apiDomain = strings.TrimSuffix(hcpApiDomain, "/")
+
+	url := fmt.Sprintf("https://%s/consul/%s/versions", apiDomain, HCPConsulAPIVersion)
+	client := http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyFromEnvironment,
+		},
+	}
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("User-Agent", "terraform-provider-hcs")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("unable to retrieve avaialable Consul versions from HCP: %+v", err)
+	}
+	var availableVersionsBody availableVersionsResponse
+	if err := json.NewDecoder(resp.Body).Decode(&availableVersionsBody); err != nil {
+		return nil, fmt.Errorf("unable to deserialize versions JSON from HCP Consul service: %+v", err)
+	}
+
+	return availableVersionsBody.Versions, nil
+}
+
+// RecommendedVersion returns the recommended version of Consul
+func RecommendedVersion(versions []Version) string {
+	var defaultVersion string
+
+	for _, v := range versions {
+		defaultVersion = v.Version
+
+		if v.Status == "RECOMMENDED" {
+			return defaultVersion
+		}
+	}
+
+	return defaultVersion
+}
+
+// IsValidVersion determines that a given version string is contained within the slice of
+// available Consul versions.
+func IsValidVersion(version string, versions []Version) bool {
+	for _, v := range versions {
+		if version == v.Version {
+			return true
+		}
+	}
+
+	return false
+}
 
 // NormalizeVersion ensures the version starts with a 'v'
 func NormalizeVersion(version string) string {
--- internal/consul/version_test.go
+++ internal/consul/version_test.go
@@ -6,6 +6,116 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func Test_RecommendedVersion(t *testing.T) {
+	tcs := map[string]struct {
+		expected string
+		input    []Version
+	}{
+		"with a recommended version": {
+			input: []Version{
+				{
+					Version: "v1.9.0",
+					Status:  "RECOMMENDED",
+				},
+				{
+					Version: "v1.8.6",
+					Status:  "AVAILABLE",
+				},
+				{
+					Version: "v1.8.4",
+					Status:  "AVAILABLE",
+				},
+			},
+			expected: "v1.9.0",
+		},
+		"without a recommended version": {
+			input: []Version{
+				{
+					Version: "v1.9.0",
+					Status:  "AVAILABLE",
+				},
+				{
+					Version: "v1.8.6",
+					Status:  "AVAILABLE",
+				},
+				{
+					Version: "v1.8.4",
+					Status:  "AVAILABLE",
+				},
+			},
+			expected: "v1.8.4",
+		},
+	}
+
+	for n, tc := range tcs {
+		t.Run(n, func(t *testing.T) {
+			r := require.New(t)
+
+			result := RecommendedVersion(tc.input)
+			r.Equal(tc.expected, result)
+		})
+	}
+}
+
+func Test_IsValidVersion(t *testing.T) {
+	tcs := map[string]struct {
+		expected      bool
+		version       string
+		validVersions []Version
+	}{
+		"with a valid version": {
+			version: "v1.9.0",
+			validVersions: []Version{
+				{
+					Version: "v1.9.0",
+					Status:  "RECOMMENDED",
+				},
+				{
+					Version: "v1.8.6",
+					Status:  "AVAILABLE",
+				},
+				{
+					Version: "v1.8.4",
+					Status:  "AVAILABLE",
+				},
+			},
+			expected: true,
+		},
+		"with an invalid version": {
+			version: "v1.8.0",
+			validVersions: []Version{
+				{
+					Version: "v1.9.0",
+					Status:  "RECOMMENDED",
+				},
+				{
+					Version: "v1.8.6",
+					Status:  "AVAILABLE",
+				},
+				{
+					Version: "v1.8.4",
+					Status:  "AVAILABLE",
+				},
+			},
+			expected: false,
+		},
+		"with no valid versions": {
+			version:       "v1.8.0",
+			validVersions: nil,
+			expected:      false,
+		},
+	}
+
+	for n, tc := range tcs {
+		t.Run(n, func(t *testing.T) {
+			r := require.New(t)
+
+			result := IsValidVersion(tc.version, tc.validVersions)
+			r.Equal(tc.expected, result)
+		})
+	}
+}
+
 func Test_NormalizeVersion(t *testing.T) {
 	tcs := map[string]struct {
 		expected string
