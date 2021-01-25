2e424b0ea28df64212ea94d7e51b6117944b0c8e
Janet Kenmotsu <jskenmotsu@gmail.com>
2021-01-25 14:36:32 -0800

    clean up and error paths

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -2,7 +2,7 @@ package provider
 
 import (
 	"context"
-	"log"
+	"fmt"
 
 	"github.com/hashicorp/cloud-sdk-go/clients/cloud-resource-manager/preview/2019-12-10/client/organization_service"
 	"github.com/hashicorp/cloud-sdk-go/clients/cloud-resource-manager/preview/2019-12-10/client/project_service"
@@ -56,8 +56,7 @@ func configure(p *schema.Provider) func(context.Context, *schema.ResourceData) (
 
 		project, err := getProject(ctx, clientID, clientSecret)
 		if err != nil {
-			// TODO figure out how to bubble up this error
-			return nil, diag.Errorf("unable to determine project and organization ID from configured credentials")
+			return nil, diag.FromErr(err)
 		}
 
 		// Construct a new HCP api client with clients and configuration.
@@ -79,72 +78,34 @@ func configure(p *schema.Provider) func(context.Context, *schema.ResourceData) (
 // return pointer to HashicorpCloudResourcemanagerProject and err (2 tuple return)
 // HashicorpCloudResourcemanagerResourceID is the org id because only orgs are allowed to be project parents currently, although it may change in the future
 func getProject(ctx context.Context, clientID string, clientSecret string) (*models.HashicorpCloudResourcemanagerProject, error) {
-	// func getProject(ctx context.Context, clientID string, clientSecret string) {
-	log.Printf("**********")
-	log.Printf("in getProject")
-	log.Printf("**********")
-
-	// if d.Get("organization_id").(string) == "" {
-
-	// TODO: drop debug prints
-	log.Printf("**********")
-	log.Printf("[HEY!!!] Initializing Client with id %s and secret %s", clientID, clientSecret)
-	log.Printf("**********")
-
-	cl, _ := clients.NewClient(clients.ClientConfig{
+	cl, err := clients.NewClient(clients.ClientConfig{
 		ClientID:     clientID,
 		ClientSecret: clientSecret,
 	})
-	// if err != nil {
-	// 	return diag.Errorf("unable to create HCP api client: %v", err)
-	// }
+	if err != nil {
+		return nil, fmt.Errorf("unable to create HCP api client: %v", err)
+	}
 
 	listOrgParams := organization_service.NewOrganizationServiceListParams()
-	listOrgResp, _ := cl.Organization.OrganizationServiceList(listOrgParams, nil)
-	log.Printf("**********")
-	log.Printf("[HEY!!!] Org List Resp %+v", listOrgResp)
-	log.Printf("**********")
-
-	// if err != nil {
-	// 	return diag.Errorf("unable to fetch organization list: %+v", err)
-	// }
+	listOrgResp, err := cl.Organization.OrganizationServiceList(listOrgParams, nil)
+	if err != nil {
+		return nil, fmt.Errorf("unable to fetch organization list: %+v", err)
+	}
 
 	// Service principles, from which the client credentials are obtained, are scoped to a single org,
 	// so this list should never have more than one org.
 	orgID := listOrgResp.Payload.Organizations[0].ID
-	log.Printf("**********")
-	log.Printf("[HEY!!!] Org ID %s", orgID)
-	log.Printf("**********")
 
-	// if err := d.Set("organization_id", orgID); err != nil {
-	// 	return diag.Errorf("unable to set organization_id: %+v", err)
-	// }
-	// }
 	listProjParams := project_service.NewProjectServiceListParams()
 	listProjParams.ScopeID = &orgID
 	scopeType := "ORGANIZATION" // TODO use the enum
 	listProjParams.ScopeType = &scopeType
 
-	listProjResp, _ := cl.Project.ProjectServiceList(listProjParams, nil)
-	log.Printf("**********")
-	log.Printf("[HEY!!!] Proj List Resp %+v", listProjResp)
-	log.Printf("**********")
+	listProjResp, err := cl.Project.ProjectServiceList(listProjParams, nil)
+	if err != nil {
+		return nil, fmt.Errorf("unable to fetch project id: %+v", err)
+	}
 
 	project := listProjResp.Payload.Projects[0]
-	log.Printf("**********")
-	log.Printf("[HEY!!!] Project %v", project)
-	log.Printf("**********")
-
-	projID := listProjResp.Payload.Projects[0].ID
-	log.Printf("**********")
-	log.Printf("[HEY!!!] Proj ID %s", projID)
-	log.Printf("**********")
-
 	return project, nil
-
-	// if err != nil {
-	// 	return nil, diag.Errorf("unable to fetch project list: %+v", err)
-	// }
-
-	// return diagnostics
 }
