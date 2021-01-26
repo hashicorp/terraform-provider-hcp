c0dbbb8f498218229ccbd98dcee0c15b4a179e2f
Brenna Hewer-Darroch <brenna@hashicorp.com>
2021-01-26 10:40:06 -0800

    update config examples dropped the client id and secret from all except the auth example to encourage use of environment variables

--- examples/guides/quick_start/_config.tf
+++ examples/guides/quick_start/_config.tf
@@ -1,13 +1,10 @@
 terraform {
   required_providers {
     hcp = {
-      source  = "localhost/providers/hcp"
-      version = "0.0.1"
+      source  = "hashicorp/hcp"
+      version = "~> 0.1.0"
     }
   }
 }
 
-provider "hcp" {
-  client_id     = "service-principal-key-client-id"
-  client_secret = "service-principal-key-client-secret"
-}
+provider "hcp" {}
