24c82cc4411c627bf7a4f375b49b28d9d9f318c2
Brenna Hewer-Darroch <brenna@hashicorp.com>
2021-01-25 18:35:52 -0800

    complete quickstart guide

--- examples/guides/quick_start/main.tf
+++ examples/guides/quick_start/main.tf
@@ -1,24 +1,22 @@
-# TODO defaults only, add comment
-resource "hcp_hvn" "example" {
-  hvn_id         = var.hvn_id
-  cloud_provider = var.cloud_provider
-  region         = var.region
+resource "hcp_hvn" "example_hvn" {
+  hvn_id         = "hcp-tf-example-hvn"
+  cloud_provider = "aws"
+  region         = "us-west-2"
+  cidr_block     = "172.25.16.0/20"
 }
 
-# TODO defaults only, add comment
-resource "hcp_consul_cluster" "example" {
-  hvn_id         = hcp_hvn.example.hvn_id
-  cluster_id     = var.cluster_id
-  cloud_provider = var.cloud_provider
-  region         = var.region
+resource "hcp_consul_cluster" "example_consul_cluster" {
+  hvn_id         = hcp_hvn.example_hvn.hvn_id
+  cluster_id     = "hcp-tf-example-consul-cluster"
+  cloud_provider = "aws"
+  region         = "us-west-2"
 }
 
-# TODO defaults only, add comment
 resource "hcp_aws_network_peering" "example_peering" {
   hvn_id                = hcp_hvn.example_hvn.hvn_id
 
-  target_vpc_id         = aws_vpc.main.id
-  target_account_id     = aws_vpc.main.owner_id
-  target_vpc_region     = data.aws_arn.main.region
-  target_vpc_cidr_block = aws_vpc.main.cidr_block
+  target_vpc_id         = "vpc-2f09a348"
+  target_account_id     = "1234567890"
+  target_vpc_region     = "us-west-2"
+  target_vpc_cidr_block = "10.0.1.0/24"
 }
