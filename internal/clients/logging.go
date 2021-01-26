a849f2e49cd3176c9eecaea455b14f049fcdc341
Alex Munda <amunda@hashicorp.com>
2021-01-26 11:18:16 -0600

    Fix logging

--- internal/clients/logging.go
+++ internal/clients/logging.go
@@ -14,12 +14,12 @@ type logger struct{}
 
 // Printf prints an info log
 func (l logger) Printf(format string, args ...interface{}) {
-	log.Printf("[INFO] %s", fmt.Sprintf(format, args))
+	log.Printf("[INFO] %s", fmt.Sprintf(format, args...))
 }
 
 // Debugf prints a debug log
 func (l logger) Debugf(format string, args ...interface{}) {
-	log.Printf("[DEBUG] %s", fmt.Sprintf(format, args))
+	log.Printf("[DEBUG] %s", fmt.Sprintf(format, args...))
 }
 
 // ShouldLog determines if TF_LOG is set to a valid level.
