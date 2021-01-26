e07dbe75e88244c74e6c6548f80c8f12287eb697
Alex Munda <amunda@hashicorp.com>
2021-01-26 16:10:21 -0600

    Peering guide

--- examples/guides/peering/_config.tf
+++ examples/guides/peering/_config.tf
@@ -1,13 +1,5 @@
-terraform {
-  required_providers {
-    hcp = {
-      source  = "localhost/providers/hcp"
-      version = "0.0.1"
-    }
-  }
-}
+provider "hcp" {}
 
-provider "hcp" {
-  client_id     = "service-principal-key-client-id"
-  client_secret = "service-principal-key-client-secret"
+provider "aws" {
+  region = "us-east-1"
 }
