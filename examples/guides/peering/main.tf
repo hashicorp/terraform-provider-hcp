6818a2446176d4c929d2892d8bf865a307438dfb
Brenna Hewer-Darroch <brenna@hashicorp.com>
2021-01-26 10:32:38 -0800

    peering attributes: prefix target_ -> peer_

--- examples/guides/peering/main.tf
+++ examples/guides/peering/main.tf
@@ -17,8 +17,8 @@ resource "hcp_consul_cluster" "example" {
 resource "hcp_aws_network_peering" "example_peering" {
   hvn_id = hcp_hvn.example_hvn.hvn_id
 
-  target_vpc_id         = aws_vpc.main.id
-  target_account_id     = aws_vpc.main.owner_id
-  target_vpc_region     = data.aws_arn.main.region
-  target_vpc_cidr_block = aws_vpc.main.cidr_block
+  peer_vpc_id         = aws_vpc.main.id
+  peer_account_id     = aws_vpc.main.owner_id
+  peer_vpc_region     = data.aws_arn.main.region
+  peer_vpc_cidr_block = aws_vpc.main.cidr_block
 }
