fadb47e77649d2f48f2c15d7d65f30046608eece
Brenna Hewer-Darroch <brenna@hashicorp.com>
2021-01-25 18:38:56 -0800

    autogen whitespace change

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -33,7 +33,7 @@ resource "aws_vpc_peering_connection_accepter" "main" {
 
 // Create a network peering connection between the HVN and the VPC
 resource "hcp_aws_network_peering" "example_peering" {
-  hvn_id                = hcp_hvn.example_hvn.hvn_id
+  hvn_id = hcp_hvn.example_hvn.hvn_id
 
   target_vpc_id         = aws_vpc.main.id
   target_account_id     = aws_vpc.main.owner_id
