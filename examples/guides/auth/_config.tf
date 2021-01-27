fdac007ac12bd0a22a8472f0f253e8019ec7639a
Brenna Hewer-Darroch <brenna@hashicorp.com>
2021-01-26 17:52:55 -0800

    drop provider version pin block from all examples except the index provider example

--- examples/guides/auth/_config.tf
+++ examples/guides/auth/_config.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    hcp = {
-      source  = "hashicorp/hcp"
-    }
-  }
-}
-
 // Credentials can be set explicitly or via the environment variables HCP_CLIENT_ID and HCP_CLIENT_SECRET
 provider "hcp" {
   client_id     = "service-principal-key-client-id"
