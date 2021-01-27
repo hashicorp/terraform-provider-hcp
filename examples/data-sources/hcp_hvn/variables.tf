6ab9b7b454cc6452f8bdef341d7a339fe2f2ed9d
Andrew Gray <andrew.gray@hashicorp.com>
2021-01-27 09:26:17 -0600

    PR feedback: drop unneeded vars from example for HVN data source

--- examples/data-sources/hcp_hvn/variables.tf
+++ examples/data-sources/hcp_hvn/variables.tf
@@ -2,13 +2,3 @@ variable "hvn_id" {
   description = "The ID of the HCP HVN."
   type        = string
 }
-
-variable "region" {
-  description = "The region of the HCP HVN."
-  type        = string
-}
-
-variable "cloud_provider" {
-  description = "The cloud provider of the HCP HVN."
-  type        = string
-}
