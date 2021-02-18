49b20663f4bb0231dfad72c1593196dd5ead8377
Riddhi Shah <riddhi@hashicorp.com>
2021-02-18 15:50:34 -0800

    Update docs

--- examples/guides/consul_cluster_federation/main.tf
+++ examples/guides/consul_cluster_federation/main.tf
@@ -1,19 +1,18 @@
 resource "hcp_hvn" "example" {
-  hvn_id         = "hvn"
-  cloud_provider = "aws"
-  region         = "us-west-2"
-  cidr_block     = "172.25.16.0/20"
+  hvn_id         = var.hvn_id
+  cloud_provider = var.cloud_provider
+  region         = var.region
 }
 
 resource "hcp_consul_cluster" "primary" {
   hvn_id     = hcp_hvn.example.hvn_id
-  cluster_id = "consul-cluster-primary"
+  cluster_id = var.primary_cluster_id
   tier       = "development"
 }
 
 resource "hcp_consul_cluster" "secondary" {
   hvn_id       = hcp_hvn.example.hvn_id
-  cluster_id   = "consul-cluster-secondary"
+  cluster_id   = var.secondary_cluster_id
   tier         = "development"
   primary_link = hcp_consul_cluster.primary.self_link
 }
