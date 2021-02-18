c3c0fa914aeb11f9c1430cbbb11805aa026f48ba
Riddhi Shah <riddhi@hashicorp.com>
2021-02-18 14:37:00 -0800

    Fix case 2
    Co-authored-by: Brenna Hewer-Darroch <brenna@hashicorp.com>

--- internal/provider/data_source_consul_cluster.go
+++ internal/provider/data_source_consul_cluster.go
@@ -123,7 +123,7 @@ func dataSourceConsulCluster() *schema.Resource {
 				Computed:    true,
 			},
 			"primary_link": {
-				Description: "The `self_link` of the HCP Consul Cluster which is the primary in the federation setup with this HCP Consul cluster. If not specified, it is a standalone cluster.",
+				Description: "The `self_link` of the HCP Consul cluster which is the primary in the federation setup with this HCP Consul cluster. If not specified, it is a standalone cluster.",
 				Type:        schema.TypeString,
 				Computed:    true,
 			},
