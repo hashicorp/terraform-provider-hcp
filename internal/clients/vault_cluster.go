388f34c89b6e4a96f0a8e15eafaf3ba1d811bd75
Brenna Hewer-Darroch <brenna@hashicorp.com>
2021-03-24 16:23:23 -0700

    add CreateVaultCluster helper

--- internal/clients/vault_cluster.go
+++ internal/clients/vault_cluster.go
@@ -8,7 +8,7 @@ import (
 	vaultmodels "github.com/hashicorp/hcp-sdk-go/clients/cloud-vault-service/preview/2020-11-25/models"
 )
 
-// GetVaultClusterByID gets an Vault cluster by its ID
+// GetVaultClusterByID gets an Vault cluster by its ID.
 func GetVaultClusterByID(ctx context.Context, client *Client, loc *sharedmodels.HashicorpCloudLocationLocation,
 	vaultClusterID string) (*vaultmodels.HashicorpCloudVault20201125Cluster, error) {
 
@@ -24,4 +24,24 @@ func GetVaultClusterByID(ctx context.Context, client *Client, loc *sharedmodels.
 	}
 
 	return getResp.Payload.Cluster, nil
+}
+
+// CreateVaultCluster will make a call to the Consul service to initiate the create Consul
+// cluster workflow.
+func CreateVaultCluster(ctx context.Context, client *Client, loc *sharedmodels.HashicorpCloudLocationLocation,
+	vaultCluster *vaultmodels.HashicorpCloudVault20201125InputCluster) (*vaultmodels.HashicorpCloudVault20201125CreateResponse, error) {
+
+	p := vault_service.NewCreateParams()
+	p.Context = ctx
+	p.Body = &vaultmodels.HashicorpCloudVault20201125CreateRequest{Cluster: vaultCluster}
+
+	p.ClusterLocationOrganizationID = loc.OrganizationID
+	p.ClusterLocationProjectID = loc.ProjectID
+
+	resp, err := client.Vault.Create(p, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Payload, nil
 }
