6893f6333fc981ea2c78ee5733d63ce4215db98f
Brenna Hewer-Darroch <brenna@hashicorp.com>
2021-03-25 14:36:38 -0700

    extract consul's version normalizer into new input package to be shared across resources

---
+++ internal/input/input.go
@@ -0,0 +1,10 @@
+package input
+
+import (
+	"strings"
+)
+
+// NormalizeVersion ensures the version starts with a 'v'
+func NormalizeVersion(version string) string {
+	return "v" + strings.TrimPrefix(version, "v")
+}
