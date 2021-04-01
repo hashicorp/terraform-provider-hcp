3f1aa0916a9c7bae4db1ba5dd191f96085299039
Brenna Hewer-Darroch <brenna@hashicorp.com>
2021-03-31 17:06:03 -0700

    add vault cluster data source

---
+++ internal/provider/data_source_vault_cluster.go
@@ -0,0 +1,130 @@
+package provider
+
+import (
+	"context"
+	"log"
+
+	sharedmodels "github.com/hashicorp/hcp-sdk-go/clients/cloud-shared/v1/models"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/hashicorp/terraform-provider-hcp/internal/clients"
+)
+
+func dataSourceVaultCluster() *schema.Resource {
+	return &schema.Resource{
+		Description: "The cluster data source provides information about an existing HCP Vault cluster.",
+		ReadContext: dataSourceVaultClusterRead,
+		Timeouts: &schema.ResourceTimeout{
+			Default: &defaultClusterTimeoutDuration,
+		},
+		Schema: map[string]*schema.Schema{
+			// Required inputs
+			"cluster_id": {
+				Description:      "The ID of the HCP Vault cluster.",
+				Type:             schema.TypeString,
+				Required:         true,
+				ValidateDiagFunc: validateSlugID,
+			},
+			// computed outputs
+			"hvn_id": {
+				Description: "The ID of the HVN this HCP Vault cluster is associated to.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"public_endpoint": {
+				Description: "Denotes that the cluster has a public endpoint. Defaults to false.",
+				Type:        schema.TypeBool,
+				Computed:    true,
+			},
+			"min_vault_version": {
+				Description: "The minimum Vault version to use when creating the cluster. If not specified, it is defaulted to the version that is currently recommended by HCP.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"tier": {
+				Description: "The tier that the HCP Vault cluster will be provisioned as.  Only 'development' is available at this time.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"organization_id": {
+				Description: "The ID of the organization this HCP Vault cluster is located in.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"project_id": {
+				Description: "The ID of the project this HCP Vault cluster is located in.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"cloud_provider": {
+				Description: "The provider where the HCP Vault cluster is located.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"region": {
+				Description: "The region where the HCP Vault cluster is located.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"namespace": {
+				Description: "The name of the customer namespace this HCP Vault cluster is located in.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"vault_version": {
+				Description: "The Vault version of the cluster.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"vault_public_endpoint_url": {
+				Description: "The public URL for the Vault cluster. This will be empty if `public_endpoint` is `false`.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"vault_private_endpoint_url": {
+				Description: "The private URL for the Vault cluster.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"created_at": {
+				Description: "The time that the Vault cluster was created.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+		},
+	}
+}
+
+func dataSourceVaultClusterRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	clusterID := d.Get("cluster_id").(string)
+	client := meta.(*clients.Client)
+
+	loc := &sharedmodels.HashicorpCloudLocationLocation{
+		OrganizationID: client.Config.OrganizationID,
+		ProjectID:      client.Config.ProjectID,
+	}
+
+	log.Printf("[INFO] Reading Vault cluster (%s) [project_id=%s, organization_id=%s]", clusterID, loc.ProjectID, loc.OrganizationID)
+
+	cluster, err := clients.GetVaultClusterByID(ctx, client, loc, clusterID)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	// build the id for this Vault cluster
+	link := newLink(loc, VaultClusterResourceType, clusterID)
+	url, err := linkURL(link)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(url)
+
+	// Cluster found, update resource data.
+	if err := setVaultClusterResourceData(d, cluster); err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
