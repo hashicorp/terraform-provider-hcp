fa10e8e10db27acc094c3379382e661bb94152ed
Ti Zhang <tizhang@hashicorp.com>
2021-04-28 15:45:18 -0700

    Replace switch case in setting target link for HVN route data source

--- internal/provider/resource_hvn_route.go
+++ internal/provider/resource_hvn_route.go
@@ -1,7 +1,6 @@
 package provider
 
 import (
-	"errors"
 	"time"
 
 	networkmodels "github.com/hashicorp/hcp-sdk-go/clients/cloud-network/preview/2020-09-07/models"
@@ -26,29 +25,10 @@ func setHVNRouteResourceData(d *schema.ResourceData, route *networkmodels.Hashic
 	}
 
 	// Set self_link identifying the target of the HVN route.
-	var targetLink string
-
-	switch route.Target.HvnConnection.Type {
-	case HvnResourceType:
-		hvnLink := newLink(loc, HvnResourceType, route.Target.HvnConnection.ID)
-		targetLink, err = linkURL(hvnLink)
-		if err != nil {
-			return err
-		}
-	case PeeringResourceType:
-		peeringLink := newLink(loc, PeeringResourceType, route.Target.HvnConnection.ID)
-		targetLink, err = linkURL(peeringLink)
-		if err != nil {
-			return err
-		}
-	case TgwAttachmentResourceType:
-		tgwAttLink := newLink(loc, PeeringResourceType, route.Target.HvnConnection.ID)
-		targetLink, err = linkURL(tgwAttLink)
-		if err != nil {
-			return err
-		}
-	default:
-		return errors.New("Unable to set self_link identifying the target - HVN Route target is not a known type.")
+	hvnLink := newLink(loc, route.Target.HvnConnection.Type, route.Target.HvnConnection.ID)
+	targetLink, err := linkURL(hvnLink)
+	if err != nil {
+		return err
 	}
 
 	if err := d.Set("target", map[string]interface{}{"self_link": targetLink}); err != nil {
