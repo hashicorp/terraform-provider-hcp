235d72ee7481a081e4a3232726041677b6023c0a
Anton Panferov <smaant@gmail.com>
2021-05-20 10:54:14 -0700

    Add peering to example for hvn route, regenerate docs

--- examples/resources/hcp_hvn_route/resource.tf
+++ examples/resources/hcp_hvn_route/resource.tf
@@ -9,6 +9,7 @@ resource "hcp_hvn" "main" {
   cidr_block     = "172.25.16.0/20"
 }
 
+// Creating an AWS transit gateway attachment and a route for it.
 resource "aws_vpc" "example" {
   cidr_block = "172.31.0.0/16"
 }
@@ -51,9 +52,34 @@ resource "aws_ec2_transit_gateway_vpc_attachment_accepter" "example" {
 }
 
 
-resource "hcp_hvn_route" "tgwroute" {
+resource "hcp_hvn_route" "example-tgw-att-hvn-route" {
   hvn              = hcp_hvn.main.self_link
   destination_cidr = aws_vpc.example.cidr_block
-  hvn_route_id     = "exampletgwhvnroute"
+  hvn_route_id     = "tgw-hvn-route"
   target_link      = hcp_aws_transit_gateway_attachment.example.self_link
+}
+
+// Creating a peering and a route for it.
+resource "aws_vpc" "peer" {
+  cidr_block = "192.168.0.0/20"
+}
+
+resource "hcp_aws_network_peering" "example" {
+  peering_id      = "peer-example"
+  hvn_id          = hcp_hvn.main.hvn_id
+  peer_vpc_id     = aws_vpc.peer.id
+  peer_account_id = aws_vpc.peer.owner_id
+  peer_vpc_region = "us-west-2"
+}
+
+resource "aws_vpc_peering_connection_accepter" "peer" {
+  vpc_peering_connection_id = hcp_aws_network_peering.example.provider_peering_id
+  auto_accept               = true
+}
+
+resource "hcp_hvn_route" "example-peering-route" {
+  hvn              = hcp_hvn.main.self_link
+  destination_cidr = aws_vpc.peer.cidr_block
+  hvn_route_id     = "peering-route"
+  target_link      = hcp_aws_network_peering.example.self_link
 }
