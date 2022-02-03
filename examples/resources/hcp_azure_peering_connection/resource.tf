26ba28c00ab6c623110b2153488c25b6cabbaae1
Brenna Hewer-Darroch <21015366+bcmdarroch@users.noreply.github.com>
2022-02-02 17:02:28 -0800

    add azure_peering resource doc

---
+++ examples/resources/hcp_azure_peering_connection/resource.tf
@@ -0,0 +1,73 @@
+resource "hcp_hvn" "hvn" {
+  hvn_id         = "main-hvn"
+  cloud_provider = "azure"
+  region         = "westus2"
+  cidr_block     = "172.25.16.0/20"
+}
+
+resource "hcp_azure_peering_connection" "peer" {
+  hvn_link                 = hcp_hvn.hvn.self_link
+  peering_id               = "dev"
+  peer_vnet_name           = azurerm_virtual_network.vnet.name
+  peer_subscription_id     = "subscription-uuid"
+  peer_tenant_id           = "tenant-uuid"
+  peer_resource_group_name = azurerm_resource_group.rg.name
+  peer_vnet_region         = hcp_hvn.hvn.region
+}
+
+resource "hcp_peering_connection_activation" "activation" {
+  peering_id = hcp_azure_peering_connection.peer.peering_id
+  hvn_link   = hcp_hvn.hvn.self_link
+}
+
+resource "hcp_hvn_route" "route" {
+  hvn_link         = hcp_hvn.hvn.self_link
+  hvn_route_id     = "azure-route"
+  destination_cidr = "172.31.0.0/16"
+  target_link      = hcp_azure_peering_connection.peer.self_link
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "resource-group-test"
+  location = "West US"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-test"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  address_space = [
+    "10.0.0.0/16"
+  ]
+}
+
+resource "azuread_service_principal" "principal" {
+  application_id = hcp_azure_peering_connection.peer.application_id
+}
+
+resource "azurerm_role_definition" "definition" {
+  name  = "hcp-hvn-peering-access"
+  scope = azurerm_virtual_network.vnet.id
+
+  assignable_scopes = [
+    azurerm_virtual_network.vnet.id
+  ]
+
+  permissions {
+    actions = [
+      "Microsoft.Network/virtualNetworks/peer/action",
+      "Microsoft.Network/virtualNetworks/virtualNetworkPeerings/write"
+    ]
+  }
+}
+
+resource "azurerm_role_assignment" "assignment" {
+  principal_id       = azuread_service_principal.principal.id
+  scope              = azurerm_virtual_network.vnet.id
+  role_definition_id = azurerm_role_definition.definition.role_definition_resource_id
+}
