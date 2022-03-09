871df9dd70cc1331cee88d3e88f0efa0bbe3c591
Chris Piraino <cpiraino@hashicorp.com>
2022-03-09 10:36:15 -0600

    HCS-2849: Do not suppress min_consul_version on creation (#252)
    * HCS-2849: Do not suppress min_consul_version on creation
    
    This commit fixes an issue where we would always suppress the
    min_consul_version on create, because a previous value for the cluster
    did not exist to compare against.
    
    * Update acceptance test to use hcp_consul_versions data source

--- internal/provider/resource_consul_cluster_test.go
+++ internal/provider/resource_consul_cluster_test.go
@@ -12,9 +12,10 @@ import (
 
 var consulCluster = `
 resource "hcp_consul_cluster" "test" {
-	cluster_id = "test-consul-cluster"
-	hvn_id     = hcp_hvn.test.hvn_id
-	tier       = "standard"
+	cluster_id         = "test-consul-cluster"
+	hvn_id             = hcp_hvn.test.hvn_id
+	tier               = "standard"
+	min_consul_version = data.hcp_consul_versions.test.recommended
 }
 `
 
@@ -35,6 +36,8 @@ func setTestAccConsulClusterConfig(consulCluster string) string {
 		region         = "us-west-2"
 	}
 
+	data "hcp_consul_versions" "test" {}
+
 	%s
 	
 	data "hcp_consul_cluster" "test" {
@@ -53,6 +56,7 @@ func setTestAccConsulClusterConfig(consulCluster string) string {
 func TestAccConsulCluster(t *testing.T) {
 	resourceName := "hcp_consul_cluster.test"
 	dataSourceName := "data.hcp_consul_cluster.test"
+	dataSourceVersionName := "data.hcp_consul_versions.test"
 	rootTokenResourceName := "hcp_consul_cluster_root_token.test"
 
 	resource.Test(t, resource.TestCase{
@@ -81,7 +85,7 @@ func TestAccConsulCluster(t *testing.T) {
 					resource.TestCheckResourceAttrSet(resourceName, "project_id"),
 					resource.TestCheckResourceAttrSet(resourceName, "consul_config_file"),
 					resource.TestCheckResourceAttrSet(resourceName, "consul_ca_file"),
-					resource.TestCheckResourceAttrSet(resourceName, "consul_version"),
+					resource.TestCheckResourceAttrPair(resourceName, "consul_version", dataSourceVersionName, "recommended"),
 					resource.TestCheckNoResourceAttr(resourceName, "consul_public_endpoint_url"),
 					resource.TestCheckResourceAttrSet(resourceName, "consul_private_endpoint_url"),
 					testAccCheckFullURL(resourceName, "consul_private_endpoint_url", ""),
