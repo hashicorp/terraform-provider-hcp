6b2235f335c5474fce13f25ffe4154a86f91da32
Justin DeFrank <justin.defrank@gmail.com>
2022-08-26 16:36:21 -0400

    Adding support for HCP Boundary (#378)
    * Adding support for HCP Boundary
    
    * Fixing a diag.Errorf()
    
    * Adding acceptance tests
    
    * HCP Boundary cluster docs
    
    * Updating consul pkg to stable channel so it will build
    
    * Added note warning of passwords in state and logs
    
    * Added additional validation for username and password, as well as updated the docs to reflect constraints.
    
    * Bumped the Boundary cluster create timeout down to 25
    
    * Added computed outputs comment to schema code for faster parsing
    
    * updating comment WRT regionality.
    
    Co-authored-by: rizkybiz <justin.defrank@hashicorp.com>

---
+++ examples/resources/hcp_boundary_cluster/import.sh
@@ -0,0 +1,2 @@
+# The import ID is {cluster_id}
+terraform import hcp_boundary_cluster.example boundary-cluster
---
+++ internal/clients/boundary_cluster.go
@@ -0,0 +1,66 @@
+package clients
+
+import (
+	"context"
+
+	"github.com/hashicorp/hcp-sdk-go/clients/cloud-boundary-service/preview/2021-12-21/client/boundary_service"
+	boundarymodels "github.com/hashicorp/hcp-sdk-go/clients/cloud-boundary-service/preview/2021-12-21/models"
+	sharedmodels "github.com/hashicorp/hcp-sdk-go/clients/cloud-shared/v1/models"
+)
+
+// GetBoundaryClusterByID gets a Boundary cluster by its ID.
+func GetBoundaryClusterByID(ctx context.Context, client *Client, loc *sharedmodels.HashicorpCloudLocationLocation,
+	boundaryClusterID string) (*boundarymodels.HashicorpCloudBoundary20211221Cluster, error) {
+
+	getParams := boundary_service.NewGetParams()
+	getParams.Context = ctx
+	getParams.ClusterID = boundaryClusterID
+	getParams.LocationOrganizationID = loc.OrganizationID
+	getParams.LocationProjectID = loc.ProjectID
+
+	getResp, err := client.Boundary.Get(getParams, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return getResp.Payload.Cluster, nil
+}
+
+// CreateBoundaryCluster will make a call to the Boundary service to initiate the create Boundary
+// cluster workflow.
+func CreateBoundaryCluster(ctx context.Context, client *Client, loc *sharedmodels.HashicorpCloudLocationLocation,
+	boundaryCreateRequest *boundarymodels.HashicorpCloudBoundary20211221CreateRequest) (*boundarymodels.HashicorpCloudBoundary20211221CreateResponse, error) {
+
+	p := boundary_service.NewCreateParams()
+	p.Context = ctx
+	p.Body = boundaryCreateRequest
+
+	p.LocationOrganizationID = loc.OrganizationID
+	p.LocationProjectID = loc.ProjectID
+
+	resp, err := client.Boundary.Create(p, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Payload, nil
+}
+
+// DeleteBoundaryCluster will make a call to the Boundary service to initiate the delete Boundary
+// cluster workflow.
+func DeleteBoundaryCluster(ctx context.Context, client *Client, loc *sharedmodels.HashicorpCloudLocationLocation,
+	boundaryClusterID string) (*boundarymodels.HashicorpCloudBoundary20211221DeleteResponse, error) {
+
+	p := boundary_service.NewDeleteParams()
+	p.Context = ctx
+	p.ClusterID = boundaryClusterID
+	p.LocationOrganizationID = loc.OrganizationID
+	p.LocationProjectID = loc.ProjectID
+
+	deleteResp, err := client.Boundary.Delete(p, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return deleteResp.Payload, nil
+}
