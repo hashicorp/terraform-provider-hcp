7dd6906b07ec96e725b5f004849998d62ea26225
Paras Prajapati <paras.prajapati@hashicorp.com>
2022-12-13 16:40:05 -0500

    add more descriptive error

--- internal/clients/client.go
+++ internal/clients/client.go
@@ -115,7 +115,7 @@ func (cl *Client) UpdateSourceChannel(d *schema.ResourceData) (*Client, error) {
 		// Return a new client with the updated source channel
 		cl, err = NewClient(cl.Config)
 		if err != nil {
-			return cl, errors.New("failed to create new client with updated source channel")
+			return cl, fmt.Errorf("failed to create new client with updated source channel: %v", err)
 		}
 	}
 
