3547470d0ebb5b5842079ba6d91fc80328c6f545
JolisaBrownHashiCorp <jolisa.brown@hashicorp.com>
2023-02-22 10:27:58 -0500

    implement multiproject planonly acceptance test coverage

--- internal/provider/project_test.go
+++ internal/provider/project_test.go
@@ -2,28 +2,18 @@ package provider
 
 import (
 	"fmt"
+	"regexp"
 	"testing"
+	"time"
 
-	sharedmodels "github.com/hashicorp/hcp-sdk-go/clients/cloud-shared/v1/models"
+	"github.com/go-openapi/strfmt"
+	"github.com/hashicorp/hcp-sdk-go/clients/cloud-resource-manager/preview/2019-12-10/models"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	"github.com/hashicorp/terraform-provider-hcp/internal/clients"
 	"github.com/stretchr/testify/assert"
 )
 
-//have a provider instance declared that (eventually) contains a defined, existing project ID
-//using providerFactory is possible
-
-var preConfigProjectID string
-
-var testAccNoProjectResourceConfig = fmt.Sprintf(`
-resource "hcp_hvn" "test" {
-	hvn_id         = "%[1]s"
-	cloud_provider = "aws"
-	region         = "us-west-2"
-}
-`, hvnUniqueID)
-
+// Project ID is read from the resource config or provider
 func Test_GetProjectID(t *testing.T) {
 	tests := []struct {
 		name        string
@@ -35,7 +25,7 @@ func Test_GetProjectID(t *testing.T) {
 		{"resource only project defined", "proj1", "", "proj1", ""},
 		{"provider only project defined", "", "proj2", "proj2", ""},
 		{"resource and provider project defined", "proj1", "proj2", "proj1", ""},
-		{"project not defined", "", "", "", fmt.Sprintf("Project ID not defined. Verify that project ID is set either in the provider or in the resource config.")},
+		{"project not defined", "", "", "", "project ID not defined. Verify that project ID is set either in the provider or in the resource config"},
 	}
 	for _, testCase := range tests {
 		t.Run(testCase.name, func(t *testing.T) {
@@ -51,53 +41,127 @@ func Test_GetProjectID(t *testing.T) {
 	}
 }
 
-//This includes tests against HVNS that contain a project ID defined in either the resource, the config, or both
-func TestAccMultiProject(t *testing.T) {
-	resourceName := "hcp_hvn.test"
+// If project ID is not defined on the provider or resource config, the provider
+// project ID becomes the organization's oldest existing project
+func TestDetermineOldestProject(t *testing.T) {
 
+	testCases := []struct {
+		name           string
+		projArray      []*models.HashicorpCloudResourcemanagerProject
+		expectedProjID string
+	}{
+		{
+			name: "One Project",
+			projArray: []*models.HashicorpCloudResourcemanagerProject{
+				{
+					CreatedAt: strfmt.DateTime(time.Date(2010, time.November, 10, 23, 0, 0, 0, time.UTC)),
+					ID:        "proj1",
+				},
+			},
+			expectedProjID: "proj1",
+		},
+		{
+			name: "Two Projects",
+			projArray: []*models.HashicorpCloudResourcemanagerProject{
+				{
+					ID:        "proj1",
+					CreatedAt: strfmt.DateTime(time.Date(2010, time.November, 10, 23, 0, 0, 0, time.UTC)),
+				},
+				{
+					ID:        "proj2",
+					CreatedAt: strfmt.DateTime(time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)),
+				},
+			},
+			expectedProjID: "proj2",
+		},
+		{
+			name: "Three Projects",
+			projArray: []*models.HashicorpCloudResourcemanagerProject{
+				{
+					ID:        "proj1",
+					CreatedAt: strfmt.DateTime(time.Date(2010, time.November, 10, 23, 0, 0, 0, time.UTC)),
+				},
+				{
+					ID:        "proj2",
+					CreatedAt: strfmt.DateTime(time.Date(2007, time.November, 10, 23, 0, 0, 0, time.UTC)),
+				},
+				{
+					ID:        "proj3",
+					CreatedAt: strfmt.DateTime(time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)),
+				},
+			},
+			expectedProjID: "proj2",
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+
+			oldestProject := getOldestProject(testCase.projArray)
+			assert.Equal(t, testCase.expectedProjID, oldestProject.ID)
+
+		})
+
+	}
+}
+
+var (
+	hvnIDUnique = fmt.Sprintf("hcp-provider-test-%s", time.Now().Format("200601021504"))
+)
+
+var providerProjectID = "prov-project-id-invalid"
+
+func TestAccMultiProject(t *testing.T) {
 	resource.Test(t, resource.TestCase{
-		PreCheck: func() { testAccPreCheck(t, map[string]bool{"aws": false, "azure": false}) },
-		//function signature here requires a map of string to func, as defined above
+		PreCheck:          func() { testAccPreCheck(t, map[string]bool{"aws": false, "azure": false}) },
 		ProviderFactories: providerFactories,
 		CheckDestroy: func(t *terraform.State) error {
-			testAccCheckHvnDestroy(t)
-			testAccMultiProjectDestroy(t)
 			return nil
 		},
 		Steps: []resource.TestStep{
 
 			{
 				PlanOnly: true,
-				//preconfig generates a project id
-				PreConfig: func() {
-
-					client := testAccProvider.Meta().(*clients.Client)
-					loc := &sharedmodels.HashicorpCloudLocationLocation{
-						ProjectID: client.Config.ProjectID}
-
-					preConfigProjectID = loc.ProjectID
-
-				},
-				//assigns generated project id to the provider
 				Config: fmt.Sprintf(`
 				provider "hcp" {
-					project_id = %s
+					project_id = "%[1]s"
 				}
-				%s
-			`, preConfigProjectID, testAccNoProjectResourceConfig),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckHvnExists(resourceName),
-					resource.TestCheckResourceAttr(resourceName, "project_id", preConfigProjectID),
-				),
+				resource "hcp_hvn" "test" {
+					hvn_id         = "%[2]s"
+					cloud_provider = "aws"
+					region         = "us-west-2"
+				}
+				`, providerProjectID, hvnIDUnique),
+				ExpectError: regexp.MustCompile(fmt.Sprintf(`unable to fetch project "%s": could not complete request: please ensure your HCP_API_HOST, HCP_CLIENT_ID, and HCP_CLIENT_SECRET are correct`, providerProjectID)),
 			},
 		},
 	})
 
 }
 
-func testAccMultiProjectDestroy(s *terraform.State) error {
+func TestAccMultiProjectResource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t, map[string]bool{"aws": false, "azure": false}) },
+		ProviderFactories: providerFactories,
+		CheckDestroy: func(t *terraform.State) error {
+			return nil
+		},
+		Steps: []resource.TestStep{
 
-	//deletes projects associated with test provider and resource
+			{
+				PlanOnly: true,
+				Config: fmt.Sprintf(`
+				provider "hcp" {}
+				resource "hcp_hvn" "test" {
+					hvn_id         = "%[1]s"
+					project_id = "resource-project-id-invalid"
+					cloud_provider = "aws"
+					region         = "us-west-2"
+				}
+				`, hvnIDUnique),
+				ExpectError: regexp.MustCompile("Invalid project ID provided for resource"),
+			},
+		},
+	})
 
-	return nil
 }
