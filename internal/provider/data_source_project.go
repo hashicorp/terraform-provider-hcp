9fe30ee1db7e9608918007c8b425b0f7a0567dbf
delores-hashicorp <delores.tochukwudiei@hashicorp.com>
2023-05-12 14:25:32 +0200

    Implement logic to read project data source

--- internal/provider/data_source_project.go
+++ internal/provider/data_source_project.go
@@ -5,11 +5,14 @@ package provider
 
 import (
 	"context"
+	"log"
 	"time"
 
+	"github.com/hashicorp/hcp-sdk-go/clients/cloud-resource-manager/stable/2019-12-10/models"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/hashicorp/terraform-provider-hcp/internal/clients"
 )
 
 var projectDefaultTimeout = time.Minute * 1
@@ -62,5 +65,44 @@ func dataSourceProject() *schema.Resource {
 // dataSourceProjectRead is the func to implement reading of an
 // HCP project.
 func dataSourceProjectRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*clients.Client)
+
+	projectID, err := GetProjectID(d.Get("project_id").(string), client.Config.ProjectID)
+	if err != nil {
+		return diag.Errorf("unable to retrieve project ID: %v", err)
+	}
+
+	// Check for an existing project
+	log.Printf("[INFO] Reading project (%s)", projectID)
+	project, err := clients.GetProjectByID(ctx, client, projectID)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(project.ID)
+
+	if err := setProjectResourceData(d, project); err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
+
+func setProjectResourceData(d *schema.ResourceData, project *models.ResourcemanagerProject) error {
+	if err := d.Set("project_id", project.ID); err != nil {
+		return err
+	}
+	if err := d.Set("name", project.Name); err != nil {
+		return err
+	}
+	if err := d.Set("created_at", project.CreatedAt.String()); err != nil {
+		return err
+	}
+	if err := d.Set("state", project.State); err != nil {
+		return err
+	}
+	if err := d.Set("organization_id", project.Parent.ID); err != nil {
+		return err
+	}
 	return nil
 }
