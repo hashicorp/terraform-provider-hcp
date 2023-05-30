9a1d30aeebe922db09953fc2532ac2700b9b6dc8
Aidan Mundy <aidan.mundy@hashicorp.com>
2023-05-30 15:00:50 -0400

    Update individual resource docs to reflect multi-project support

--- examples/resources/hcp_vault_cluster/import.sh
+++ examples/resources/hcp_vault_cluster/import.sh
@@ -1,2 +1,6 @@
-# The import ID is {cluster_id}
+# Using an explicit project ID, the import ID is:
+# {project_id}:{cluster_id}
+terraform import hcp_vault_cluster.example f709ec73-55d4-46d8-897d-816ebba28778:vault-cluster
+# Using the provider-default project ID, the import ID is:
+# {cluster_id}
 terraform import hcp_vault_cluster.example vault-cluster
