08a0a25fd06ca62e0c32f2c66c7acfe3df2825c2
Austin Valle <austinvalle@gmail.com>
2023-06-13 09:23:55 -0400

    Add new data source for HCP Vault (#524)
    * Add new data source
    
    * add changelog

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -39,6 +39,7 @@ func New() func() *schema.Provider {
 				"hcp_hvn_peering_connection":         dataSourceHvnPeeringConnection(),
 				"hcp_hvn_route":                      dataSourceHVNRoute(),
 				"hcp_vault_cluster":                  dataSourceVaultCluster(),
+				"hcp_vault_secrets_app":              dataSourceVaultSecretsApp(),
 				"hcp_packer_image_iteration":         dataSourcePackerImageIteration(),
 				"hcp_packer_image":                   dataSourcePackerImage(),
 				"hcp_packer_iteration":               dataSourcePackerIteration(),
