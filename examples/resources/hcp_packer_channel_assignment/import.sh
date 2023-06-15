fe2b2577dc7a0a68a098d165746c909f41db7fba
Aidan Mundy <aidan.mundy@hashicorp.com>
2023-06-15 11:37:57 -0400

    Update docs

---
+++ examples/resources/hcp_packer_channel_assignment/import.sh
@@ -0,0 +1,6 @@
+# Using an explicit project ID, the import ID is:
+# {project_id}:{bucket_name}:{channel_name}
+terraform import hcp_packer_channel_assignment.staging f709ec73-55d4-46d8-897d-816ebba28778:alpine:staging
+# Using the provider-default project ID, the import ID is:
+# {bucket_name}:{channel_name}
+terraform import hcp_packer_channel_assignment.staging alpine:staging
---
+++ examples/resources/hcp_packer_channel_assignment/resource.tf
@@ -0,0 +1,34 @@
+resource "hcp_packer_channel_assignment" "staging" {
+  bucket_name  = "alpine"
+  channel_name = "staging"
+
+  # Exactly one of version, id, or fingerprint must be set:
+  iteration_version = 12
+  # iteration_id = "01H1SF9NWAK8AP25PAWDBGZ1YD"
+  # iteration_fingerprint = "01H1ZMW0Q2W6FT4FK27FQJCFG7"
+
+  # To set the channel to have no assignment, use one of the attributes with their zero value:
+  # iteration_version = 0
+  # iteration_id = ""
+  # iteration_fingerprint = ""
+}
+
+# More advanced management is possible, including
+# - Creating the channel within Terraform
+# - Assigning the channel to the latest complete iteration automatically
+resource "hcp_packer_channel" "advanced" {
+  name        = "advanced"
+  bucket_name = "alpine"
+}
+
+data "hcp_packer_iteration" "latest" {
+  bucket_name = hcp_packer_channel.advanced.bucket_name
+  channel     = "latest"
+}
+
+resource "hcp_packer_channel_assignment" "advanced" {
+  bucket_name  = hcp_packer_channel.advanced.bucket_name
+  channel_name = hcp_packer_channel.advanced.name
+
+  iteration_version = data.hcp_packer_iteration.latest.incremental_version
+}
