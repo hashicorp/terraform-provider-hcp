49f58c7d75087131de22f42f4c22e9062ab172a8
Aidan Mundy <aidan.mundy@hashicorp.com>
2023-07-19 19:14:53 -0400

    Add drift detection tests for `restricted`

--- internal/provider/resource_packer_channel_test.go
+++ internal/provider/resource_packer_channel_test.go
@@ -32,17 +32,7 @@ func TestAccPackerChannel(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testConfig(testAccConfigBuildersToString(channelConfig)),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet(channelConfig.ResourceName(), "author_id"),
-					resource.TestCheckResourceAttr(channelConfig.ResourceName(), "bucket_name", bucketSlug),
-					resource.TestCheckResourceAttrSet(channelConfig.ResourceName(), "created_at"),
-					resource.TestCheckResourceAttrSet(channelConfig.ResourceName(), "id"),
-					resource.TestCheckResourceAttr(channelConfig.ResourceName(), "name", channelSlug),
-					resource.TestCheckResourceAttrSet(channelConfig.ResourceName(), "organization_id"),
-					resource.TestCheckResourceAttrSet(channelConfig.ResourceName(), "project_id"),
-					resource.TestCheckResourceAttrSet(channelConfig.ResourceName(), "updated_at"),
-					resource.TestCheckResourceAttrSet(channelConfig.ResourceName(), "restricted"),
-				),
+				Check:  testAccCheckPackerChannel(channelConfig.ResourceName(), channelSlug, bucketSlug, ""),
 			},
 			{
 				ResourceName:      channelConfig.ResourceName(),
@@ -52,17 +42,7 @@ func TestAccPackerChannel(t *testing.T) {
 			},
 			{ // Unrestrict channel (likely a no-op)
 				Config: testConfig(testAccConfigBuildersToString(unrestrictedChannelConfig)),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet(unrestrictedChannelConfig.ResourceName(), "author_id"),
-					resource.TestCheckResourceAttr(unrestrictedChannelConfig.ResourceName(), "bucket_name", bucketSlug),
-					resource.TestCheckResourceAttrSet(unrestrictedChannelConfig.ResourceName(), "created_at"),
-					resource.TestCheckResourceAttrSet(unrestrictedChannelConfig.ResourceName(), "id"),
-					resource.TestCheckResourceAttr(unrestrictedChannelConfig.ResourceName(), "name", channelSlug),
-					resource.TestCheckResourceAttrSet(unrestrictedChannelConfig.ResourceName(), "organization_id"),
-					resource.TestCheckResourceAttrSet(unrestrictedChannelConfig.ResourceName(), "project_id"),
-					resource.TestCheckResourceAttrSet(unrestrictedChannelConfig.ResourceName(), "updated_at"),
-					resource.TestCheckResourceAttr(unrestrictedChannelConfig.ResourceName(), "restricted", "false"),
-				),
+				Check:  testAccCheckPackerChannel(unrestrictedChannelConfig.ResourceName(), channelSlug, bucketSlug, "false"),
 			},
 			{ // Validate importing explicitly unrestricted channel
 				ResourceName:      unrestrictedChannelConfig.ResourceName(),
@@ -72,17 +52,7 @@ func TestAccPackerChannel(t *testing.T) {
 			},
 			{ // Restrict channel
 				Config: testConfig(testAccConfigBuildersToString(restrictedChannelConfig)),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttrSet(restrictedChannelConfig.ResourceName(), "author_id"),
-					resource.TestCheckResourceAttr(restrictedChannelConfig.ResourceName(), "bucket_name", bucketSlug),
-					resource.TestCheckResourceAttrSet(restrictedChannelConfig.ResourceName(), "created_at"),
-					resource.TestCheckResourceAttrSet(restrictedChannelConfig.ResourceName(), "id"),
-					resource.TestCheckResourceAttr(restrictedChannelConfig.ResourceName(), "name", channelSlug),
-					resource.TestCheckResourceAttrSet(restrictedChannelConfig.ResourceName(), "organization_id"),
-					resource.TestCheckResourceAttrSet(restrictedChannelConfig.ResourceName(), "project_id"),
-					resource.TestCheckResourceAttrSet(restrictedChannelConfig.ResourceName(), "updated_at"),
-					resource.TestCheckResourceAttr(restrictedChannelConfig.ResourceName(), "restricted", "true"),
-				),
+				Check:  testAccCheckPackerChannel(restrictedChannelConfig.ResourceName(), channelSlug, bucketSlug, "true"),
 			},
 			{ // Validate importing explicitly restricted channel
 				ResourceName:      restrictedChannelConfig.ResourceName(),
@@ -97,9 +67,9 @@ func TestAccPackerChannel(t *testing.T) {
 func TestAccPackerChannel_HCPManaged(t *testing.T) {
 	bucketSlug := testAccCreateSlug("ChannelHCPManaged")
 	channelSlug := "latest"
-	channelConfig := testAccPackerChannelBuilderBase("latest", fmt.Sprintf("%q", channelSlug), fmt.Sprintf("%q", bucketSlug))
-	unrestrictedChannelConfig := testAccPackerChannelBuilderFromChannel(channelConfig, "false")
-	restrictedChannelConfig := testAccPackerChannelBuilderFromChannel(channelConfig, "true")
+	latestConfig := testAccPackerChannelBuilderBase("latest", fmt.Sprintf("%q", channelSlug), fmt.Sprintf("%q", bucketSlug))
+	unrestrictedLatestConfig := testAccPackerChannelBuilderFromChannel(latestConfig, "false")
+	restrictedLatestConfig := testAccPackerChannelBuilderFromChannel(latestConfig, "true")
 
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
@@ -114,67 +84,148 @@ func TestAccPackerChannel_HCPManaged(t *testing.T) {
 		},
 		Steps: []resource.TestStep{
 			{ // Validate "creating" (automatically adopting) a managed channel
-				Config: testConfig(testAccConfigBuildersToString(channelConfig)),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet(channelConfig.ResourceName(), "author_id"),
-					resource.TestCheckResourceAttr(channelConfig.ResourceName(), "bucket_name", bucketSlug),
-					resource.TestCheckResourceAttrSet(channelConfig.ResourceName(), "created_at"),
-					resource.TestCheckResourceAttrSet(channelConfig.ResourceName(), "id"),
-					resource.TestCheckResourceAttr(channelConfig.ResourceName(), "name", channelSlug),
-					resource.TestCheckResourceAttrSet(channelConfig.ResourceName(), "organization_id"),
-					resource.TestCheckResourceAttrSet(channelConfig.ResourceName(), "project_id"),
-					resource.TestCheckResourceAttrSet(channelConfig.ResourceName(), "updated_at"),
-					resource.TestCheckResourceAttrSet(channelConfig.ResourceName(), "restricted"),
-				),
+				Config: testConfig(testAccConfigBuildersToString(latestConfig)),
+				Check:  testAccCheckPackerChannel(latestConfig.ResourceName(), channelSlug, bucketSlug, ""),
 			},
 			{
-				ResourceName:      channelConfig.ResourceName(),
+				ResourceName:      latestConfig.ResourceName(),
 				ImportState:       true,
 				ImportStateId:     fmt.Sprintf("%s:%s", bucketSlug, channelSlug),
 				ImportStateVerify: true,
 			},
 			{ // Unrestrict managed channel
-				Config: testConfig(testAccConfigBuildersToString(unrestrictedChannelConfig)),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet(unrestrictedChannelConfig.ResourceName(), "author_id"),
-					resource.TestCheckResourceAttr(unrestrictedChannelConfig.ResourceName(), "bucket_name", bucketSlug),
-					resource.TestCheckResourceAttrSet(unrestrictedChannelConfig.ResourceName(), "created_at"),
-					resource.TestCheckResourceAttrSet(unrestrictedChannelConfig.ResourceName(), "id"),
-					resource.TestCheckResourceAttr(unrestrictedChannelConfig.ResourceName(), "name", channelSlug),
-					resource.TestCheckResourceAttrSet(unrestrictedChannelConfig.ResourceName(), "organization_id"),
-					resource.TestCheckResourceAttrSet(unrestrictedChannelConfig.ResourceName(), "project_id"),
-					resource.TestCheckResourceAttrSet(unrestrictedChannelConfig.ResourceName(), "updated_at"),
-					resource.TestCheckResourceAttr(unrestrictedChannelConfig.ResourceName(), "restricted", "false"),
-				),
+				Config: testConfig(testAccConfigBuildersToString(unrestrictedLatestConfig)),
+				Check:  testAccCheckPackerChannel(unrestrictedLatestConfig.ResourceName(), channelSlug, bucketSlug, "false"),
 			},
 			{ // Validate importing explicitly unrestricted managed channel
-				ResourceName:      unrestrictedChannelConfig.ResourceName(),
+				ResourceName:      unrestrictedLatestConfig.ResourceName(),
 				ImportState:       true,
 				ImportStateId:     fmt.Sprintf("%s:%s", bucketSlug, channelSlug),
 				ImportStateVerify: true,
 			},
 			{ // Restrict managed channel
-				Config: testConfig(testAccConfigBuildersToString(restrictedChannelConfig)),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttrSet(restrictedChannelConfig.ResourceName(), "author_id"),
-					resource.TestCheckResourceAttr(restrictedChannelConfig.ResourceName(), "bucket_name", bucketSlug),
-					resource.TestCheckResourceAttrSet(restrictedChannelConfig.ResourceName(), "created_at"),
-					resource.TestCheckResourceAttrSet(restrictedChannelConfig.ResourceName(), "id"),
-					resource.TestCheckResourceAttr(restrictedChannelConfig.ResourceName(), "name", channelSlug),
-					resource.TestCheckResourceAttrSet(restrictedChannelConfig.ResourceName(), "organization_id"),
-					resource.TestCheckResourceAttrSet(restrictedChannelConfig.ResourceName(), "project_id"),
-					resource.TestCheckResourceAttrSet(restrictedChannelConfig.ResourceName(), "updated_at"),
-					resource.TestCheckResourceAttr(restrictedChannelConfig.ResourceName(), "restricted", "true"),
-				),
+				Config: testConfig(testAccConfigBuildersToString(restrictedLatestConfig)),
+				Check:  testAccCheckPackerChannel(restrictedLatestConfig.ResourceName(), channelSlug, bucketSlug, "true"),
 			},
 			{ // Validate importing explicitly restricted managed channel
-				ResourceName:      restrictedChannelConfig.ResourceName(),
+				ResourceName:      restrictedLatestConfig.ResourceName(),
 				ImportState:       true,
 				ImportStateId:     fmt.Sprintf("%s:%s", bucketSlug, channelSlug),
 				ImportStateVerify: true,
 			},
 		},
 	})
+}
+
+func TestAccPackerChannel_RestrictionDrift(t *testing.T) {
+	bucketSlug := testAccCreateSlug("RestrictionDrift")
+	channelSlug := bucketSlug // No need for a different slug
+
+	channelUnrestrictedConfig := testAccPackerChannelBuilder("Drift", fmt.Sprintf("%q", channelSlug), fmt.Sprintf("%q", bucketSlug), "false")
+	channelRestrictedConfig := testAccPackerChannelBuilderFromChannel(channelUnrestrictedConfig, "true")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t, map[string]bool{"aws": false, "azure": false})
+			upsertRegistry(t)
+			upsertBucket(t, bucketSlug)
+		},
+		ProviderFactories: providerFactories,
+		CheckDestroy: func(*terraform.State) error {
+			deleteBucket(t, bucketSlug, true)
+			return nil
+		},
+		Steps: []resource.TestStep{
+			// Normal channels
+			{
+				Config: testConfig(testAccConfigBuildersToString(channelUnrestrictedConfig)),
+				Check:  testAccCheckPackerChannel(channelUnrestrictedConfig.ResourceName(), channelSlug, bucketSlug, "false"),
+			},
+			{ // Check drift mitigation for normal channel from false->true
+				PreConfig: func() {
+					updateChannelRestriction(t, bucketSlug, channelSlug, true)
+				},
+				Config: testConfig(testAccConfigBuildersToString(channelUnrestrictedConfig)),
+				Check:  testAccCheckPackerChannel(channelUnrestrictedConfig.ResourceName(), channelSlug, bucketSlug, "false"),
+			},
+			{
+				Config: testConfig(testAccConfigBuildersToString(channelRestrictedConfig)),
+				Check:  testAccCheckPackerChannel(channelRestrictedConfig.ResourceName(), channelSlug, bucketSlug, "true"),
+			},
+			{ // Check drift mitigation for normal channel from true->false
+				PreConfig: func() {
+					updateChannelRestriction(t, bucketSlug, channelSlug, false)
+				},
+				Config: testConfig(testAccConfigBuildersToString(channelRestrictedConfig)),
+				Check:  testAccCheckPackerChannel(channelRestrictedConfig.ResourceName(), channelSlug, bucketSlug, "true"),
+			},
+		},
+	})
+}
+
+func TestAccPackerChannel_RestrictionDriftHCPManaged(t *testing.T) {
+	bucketSlug := testAccCreateSlug("RestrictionDriftHCPManaged")
+	latestSlug := "latest"
+
+	latestUnrestrictedConfig := testAccPackerChannelBuilder("DriftHCPManaged", fmt.Sprintf("%q", latestSlug), fmt.Sprintf("%q", bucketSlug), "false")
+	latestRestrictedConfig := testAccPackerChannelBuilderFromChannel(latestUnrestrictedConfig, "true")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t, map[string]bool{"aws": false, "azure": false})
+			upsertRegistry(t)
+			upsertBucket(t, bucketSlug)
+		},
+		ProviderFactories: providerFactories,
+		CheckDestroy: func(*terraform.State) error {
+			deleteBucket(t, bucketSlug, true)
+			return nil
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: testConfig(testAccConfigBuildersToString(latestUnrestrictedConfig)),
+				Check:  testAccCheckPackerChannel(latestUnrestrictedConfig.ResourceName(), latestSlug, bucketSlug, "false"),
+			},
+			{ // Check drift mitigation for HCP managed channel from false->true
+				PreConfig: func() {
+					updateChannelRestriction(t, bucketSlug, latestSlug, true)
+				},
+				Config: testConfig(testAccConfigBuildersToString(latestUnrestrictedConfig)),
+				Check:  testAccCheckPackerChannel(latestUnrestrictedConfig.ResourceName(), latestSlug, bucketSlug, "false"),
+			},
+			{
+				Config: testConfig(testAccConfigBuildersToString(latestRestrictedConfig)),
+				Check:  testAccCheckPackerChannel(latestRestrictedConfig.ResourceName(), latestSlug, bucketSlug, "true"),
+			},
+			{ // Check drift mitigation for HCP managed channel from true->false
+				PreConfig: func() {
+					updateChannelRestriction(t, bucketSlug, latestSlug, false)
+				},
+				Config: testConfig(testAccConfigBuildersToString(latestRestrictedConfig)),
+				Check:  testAccCheckPackerChannel(latestRestrictedConfig.ResourceName(), latestSlug, bucketSlug, "true"),
+			},
+		},
+	})
+}
+
+func testAccCheckPackerChannel(resourceName string, channelName string, bucketName string, restriction string) resource.TestCheckFunc {
+	tests := []resource.TestCheckFunc{
+		resource.TestCheckResourceAttrSet(resourceName, "author_id"),
+		resource.TestCheckResourceAttr(resourceName, "bucket_name", bucketName),
+		resource.TestCheckResourceAttrSet(resourceName, "created_at"),
+		resource.TestCheckResourceAttrSet(resourceName, "id"),
+		resource.TestCheckResourceAttr(resourceName, "name", channelName),
+		resource.TestCheckResourceAttrSet(resourceName, "organization_id"),
+		resource.TestCheckResourceAttrSet(resourceName, "project_id"),
+		resource.TestCheckResourceAttrSet(resourceName, "updated_at"),
+		resource.TestCheckResourceAttrSet(resourceName, "managed"),
+	}
+	if restriction != "" {
+		tests = append(tests, resource.TestCheckResourceAttr(resourceName, "restricted", restriction))
+	} else {
+		tests = append(tests, resource.TestCheckResourceAttrSet(resourceName, "restricted"))
+	}
+	return resource.ComposeAggregateTestCheckFunc(tests...)
 }
 
 func testAccPackerChannelBuilderBase(uniqueName string, channelName string, bucketName string) testAccConfigBuilderInterface {
