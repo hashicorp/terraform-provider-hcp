a8ecfa3b8f916e1677ceac24f0332a81f1d8595a
Annalise Diroff <adiroff@hashicorp.com>
2023-08-03 12:02:16 -0700

    Test file

---
+++ internal/provider/resource_vault_secrets_app_test.go
@@ -0,0 +1,12 @@
+package provider
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+// TODO Add tests
+func TestAccFrameworkResourceApp(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{})
+}
