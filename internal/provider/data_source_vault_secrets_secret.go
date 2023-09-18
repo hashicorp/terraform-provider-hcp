99c7c97345b70bb09ff2fdc196940271f092a4b1
AnPucel <adiroff@hashicorp.com>
2023-09-18 11:20:52 -0700

    Update internal/provider/data_source_vault_secrets_secret.go
    Co-authored-by: Anton Averchenkov <84287187+averche@users.noreply.github.com>

--- internal/provider/data_source_vault_secrets_secret.go
+++ internal/provider/data_source_vault_secrets_secret.go
@@ -37,7 +37,7 @@ func (d *DataSourceVaultSecretsSecret) Metadata(ctx context.Context, req datasou
 
 func (d *DataSourceVaultSecretsSecret) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
 	resp.Schema = schema.Schema{
-		MarkdownDescription: "The Vault Secrets secret data source retrieves a singular secret and it's latest version.",
+		MarkdownDescription: "The Vault Secrets secret data source retrieves a singular secret and its latest version.",
 		Attributes: map[string]schema.Attribute{
 			"id": schema.StringAttribute{
 				Computed:    true,
