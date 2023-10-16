b604b208e60b7dd1a0c735f1d06080f885c08670
Aidan Mundy <aidan.mundy@hashicorp.com>
2023-10-16 19:53:06 -0400

    Move SDKv2 tests to use `terraform-plugin-test` (#633)
    * Move SDKv2 tests to use `terraform-plugin-test`
    
    * Import sorting

--- internal/providersdkv2/resource_consul_cluster.go
+++ internal/providersdkv2/resource_consul_cluster.go
@@ -16,7 +16,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-
 	"github.com/hashicorp/terraform-provider-hcp/internal/clients"
 	"github.com/hashicorp/terraform-provider-hcp/internal/consul"
 	"github.com/hashicorp/terraform-provider-hcp/internal/input"
