bed83755a2aaefb5937dc06f55eef9c0089afaf6
Alex Dadgar <alex.dadgar@gmail.com>
2023-10-19 15:57:04 -0700

    Workload Identity Provider resource
    Add hcp_iam_workload_identity_provider resource that allows creating
    workload identity providers. This allows associating external identities
    with HCP Service Principals.

---
+++ internal/provider/iam/resource_workload_identity_federation_test.go
@@ -0,0 +1,87 @@
+package iam_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/hashicorp/terraform-provider-hcp/internal/provider/acctest"
+)
+
+func TestAccWorkloadIdentityProviderResource(t *testing.T) {
+	accountID, accountID2 := "123456789012", "123456789098"
+	spName := acctest.RandString(16)
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		Steps: []resource.TestStep{
+			{
+				Config: testAccWorkloadIdentityProviderConfig(spName, accountID),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("hcp_iam_workload_identity_provider.example", "aws.account_id", accountID),
+					resource.TestCheckResourceAttrSet("hcp_iam_workload_identity_provider.example", "resource_name"),
+					resource.TestCheckResourceAttrSet("hcp_iam_workload_identity_provider.example", "resource_id"),
+				),
+			},
+			{
+				ResourceName:                         "hcp_iam_workload_identity_provider.example",
+				ImportState:                          true,
+				ImportStateVerifyIdentifierAttribute: "resource_name",
+				ImportStateIdFunc:                    testAccWorkloadIdentityProviderImportID,
+				ImportStateVerify:                    true,
+			},
+			{
+				// Update the account id and expect an inplace update
+				Config: testAccWorkloadIdentityProviderConfig(spName, accountID2),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction("hcp_iam_workload_identity_provider.example", plancheck.ResourceActionUpdate),
+					},
+				},
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("hcp_iam_workload_identity_provider.example", "aws.account_id", accountID2),
+					resource.TestCheckResourceAttrSet("hcp_iam_workload_identity_provider.example", "resource_name"),
+					resource.TestCheckResourceAttrSet("hcp_iam_workload_identity_provider.example", "resource_id"),
+				),
+			},
+		},
+	})
+}
+
+// testAccWorkloadIdentityProviderImportID retrieves the resource_name so that it can be imported.
+func testAccWorkloadIdentityProviderImportID(s *terraform.State) (string, error) {
+	rs, ok := s.RootModule().Resources["hcp_iam_workload_identity_provider.example"]
+	if !ok {
+		return "", fmt.Errorf("resource not found")
+	}
+
+	id, ok := rs.Primary.Attributes["resource_name"]
+	if !ok {
+		return "", fmt.Errorf("resource_name not set")
+	}
+
+	return id, nil
+}
+
+func testAccWorkloadIdentityProviderConfig(spName, accountID string) string {
+	config := `
+resource "hcp_service_principal" "example" {
+	name = %q
+}
+
+resource "hcp_iam_workload_identity_provider" "example" {
+	service_principal = hcp_service_principal.example.resource_name
+	name = "aws"
+	description = "wif"
+	conditional_access = "aws.arn == \"arn:aws:sts::%s:assumed-role/bar\""
+
+	aws = {
+		account_id = %q
+	}
+} `
+
+	return fmt.Sprintf(config, spName, accountID, accountID)
+}
