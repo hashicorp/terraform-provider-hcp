46ec29cef8afbef2b1f0a0d500679005000a2202
Alex Dadgar <alex@hashicorp.com>
2023-11-02 09:41:48 -0700

    Add Organization/Project IAM Policy/Binding resources (#661)
    * Add IAM Policy data source
    
    * Add Organization/Project IAM Policy/Binding resources
    
    Add:
    * hcp_organization_iam_policy
    * hcp_organization_iam_binding
    * hcp_project_iam_policy
    * hcp_project_iam_binding
    
    These resources are implemented through a common interface that will
    allow us to extend the IAM resources to other objects in a consistent
    and performant way.
    
    * Documentation
    
    * Add a custom type to handle the policy_data having a different order but having the same value and not producing a plan diff. Also improve package documentation and layout
    
    * Changelog entry
    
    * linter fixes

---
+++ internal/clients/iampolicy/doc.go
@@ -0,0 +1,9 @@
+// iampolicy is a helper package for creating Terraform resources that modify an
+// HCP Resource's IAM Policy. By implementing a single interface, a resource can
+// provide an authoratative Policy resource and a Binding resource. Using this
+// package simplifies implementation, provides a performance optimized
+// experience, and providers a consistent interface to the provider users.
+//
+// For examples of how to implement the interface, see
+// hcp_project_iam_policy/binding.
+package iampolicy
