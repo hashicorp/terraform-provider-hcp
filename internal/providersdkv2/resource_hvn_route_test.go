7ddf1e22002bec4f9711de547c4bae434516dca8
Jason Pilz <jasonpilz@gmail.com>
2023-12-08 11:48:21 -0800

    Prepare hvn route create test for aws and azure tests

--- internal/providersdkv2/resource_hvn_route_test.go
+++ internal/providersdkv2/resource_hvn_route_test.go
@@ -16,69 +16,75 @@ import (
 
 var (
 	// using unique names for resources to make debugging easier
-	hvnRouteUniqueAWSName = fmt.Sprintf("hcp-provider-test-%s", time.Now().Format("200601021504"))
-	testAccHvnRouteConfig = fmt.Sprintf(`
-provider "aws" {
-  region = "us-west-2"
-}
+	hvnRouteUniqueName       = fmt.Sprintf("hcp-provider-test-%s", time.Now().Format("200601021504"))
+	testAccHvnRouteConfigAws = fmt.Sprintf(`
+	  provider "aws" {
+		region = "us-west-2"
+	  }
 
-resource "hcp_hvn" "test" {
-	hvn_id         = "%[1]s"
-	cloud_provider = "aws"
-	region         = "us-west-2"
-}
+	  resource "hcp_hvn" "test" {
+		  hvn_id         = "%[1]s"
+		  cloud_provider = "aws"
+		  region         = "us-west-2"
+	  }
 
-resource "aws_vpc" "vpc" {
-  cidr_block = "10.220.0.0/16"
-  tags = {
-     Name = "%[1]s"
-  }
-}
+	  resource "aws_vpc" "vpc" {
+		cidr_block = "10.220.0.0/16"
+		tags = {
+		   Name = "%[1]s"
+		}
+	  }
 
-resource "hcp_aws_network_peering" "peering" {
-  peering_id      = "%[1]s"
-  hvn_id          = hcp_hvn.test.hvn_id
-  peer_account_id = aws_vpc.vpc.owner_id
-  peer_vpc_id     = aws_vpc.vpc.id
-  peer_vpc_region = "us-west-2"
-}
+	  resource "hcp_aws_network_peering" "peering" {
+		peering_id      = "%[1]s"
+		hvn_id          = hcp_hvn.test.hvn_id
+		peer_account_id = aws_vpc.vpc.owner_id
+		peer_vpc_id     = aws_vpc.vpc.id
+		peer_vpc_region = "us-west-2"
+	  }
 
-// This data source is the same as the resource above, but waits for the connection to be Active before returning.
-data "hcp_aws_network_peering" "peering" {
-  hvn_id                = hcp_hvn.test.hvn_id
-  peering_id            = hcp_aws_network_peering.peering.peering_id
-  wait_for_active_state = true
-}
+	  // This data source is the same as the resource above, but waits for the connection to be Active before returning.
+	  data "hcp_aws_network_peering" "peering" {
+		hvn_id                = hcp_hvn.test.hvn_id
+		peering_id            = hcp_aws_network_peering.peering.peering_id
+		wait_for_active_state = true
+	  }
 
-// The route depends on the data source, rather than the resource, to ensure the peering is in an Active state.
-resource "hcp_hvn_route" "route" {
-  hvn_route_id     = "%[1]s"
-  hvn_link         = hcp_hvn.test.self_link
-  destination_cidr = "172.31.0.0/16"
-  target_link      = data.hcp_aws_network_peering.peering.self_link
-}
+	  // The route depends on the data source, rather than the resource, to ensure the peering is in an Active state.
+	  resource "hcp_hvn_route" "route" {
+		hvn_route_id     = "%[1]s"
+		hvn_link         = hcp_hvn.test.self_link
+		destination_cidr = "172.31.0.0/16"
+		target_link      = data.hcp_aws_network_peering.peering.self_link
+	  }
 
-resource "aws_vpc_peering_connection_accepter" "peering-accepter" {
-  vpc_peering_connection_id = hcp_aws_network_peering.peering.provider_peering_id
-  auto_accept               = true
-  tags = {
-     Name = "%[1]s"
+	  resource "aws_vpc_peering_connection_accepter" "peering-accepter" {
+		vpc_peering_connection_id = hcp_aws_network_peering.peering.provider_peering_id
+		auto_accept               = true
+		tags = {
+		   Name = "%[1]s"
 
-	 // we need to have these tags here because peering-accepter will turn into
-     // an actual peering which HCP will populate with a set of tags (the ones below).
-     // After succesfull "apply"" test will try to run "plan" operation
-     // to make sure there are no changes to the state and if we don't specify these
-     // tags here then it will fail.
-	 hvn_id          = hcp_hvn.test.hvn_id
-	 organization_id = hcp_hvn.test.organization_id
-	 project_id      = hcp_hvn.test.project_id
-	 peering_id      = hcp_aws_network_peering.peering.peering_id
-  }
-}
-`, hvnRouteUniqueAWSName)
+		   // we need to have these tags here because peering-accepter will turn into
+		   // an actual peering which HCP will populate with a set of tags (the ones below).
+		   // After succesfull "apply"" test will try to run "plan" operation
+		   // to make sure there are no changes to the state and if we don't specify these
+		   // tags here then it will fail.
+		   hvn_id          = hcp_hvn.test.hvn_id
+		   organization_id = hcp_hvn.test.organization_id
+		   project_id      = hcp_hvn.test.project_id
+		   peering_id      = hcp_aws_network_peering.peering.peering_id
+		}
+	  }
+	  `, hvnRouteUniqueName)
 )
 
-func TestAccHvnRoute(t *testing.T) {
+var testAccHvnRouteConfigAzure = `
+	provider "azurerm" {
+	  features {}
+	}
+`
+
+func TestAccHvnRouteAws(t *testing.T) {
 	resourceName := "hcp_hvn_route.route"
 
 	resource.Test(t, resource.TestCase{
@@ -92,14 +98,14 @@ func TestAccHvnRoute(t *testing.T) {
 		Steps: []resource.TestStep{
 			// Testing that initial Apply created correct HVN route
 			{
-				Config: testConfig(testAccHvnRouteConfig),
+				Config: testConfig(testAccHvnRouteConfigAws),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckHvnRouteExists(resourceName),
-					resource.TestCheckResourceAttr(resourceName, "hvn_route_id", hvnRouteUniqueAWSName),
+					resource.TestCheckResourceAttr(resourceName, "hvn_route_id", hvnRouteUniqueName),
 					resource.TestCheckResourceAttr(resourceName, "destination_cidr", "172.31.0.0/16"),
 					resource.TestCheckResourceAttr(resourceName, "state", "ACTIVE"),
-					testLink(resourceName, "self_link", hvnRouteUniqueAWSName, HVNRouteResourceType, "hcp_hvn.test"),
-					testLink(resourceName, "target_link", hvnRouteUniqueAWSName, PeeringResourceType, "hcp_hvn.test"),
+					testLink(resourceName, "self_link", hvnRouteUniqueName, HVNRouteResourceType, "hcp_hvn.test"),
+					testLink(resourceName, "target_link", hvnRouteUniqueName, PeeringResourceType, "hcp_hvn.test"),
 				),
 			},
 			// Testing that we can import HVN route created in the previous step and that the
@@ -121,14 +127,72 @@ func TestAccHvnRoute(t *testing.T) {
 			},
 			// Testing running Terraform Apply for already known resource
 			{
-				Config: testConfig(testAccHvnRouteConfig),
+				Config: testConfig(testAccHvnRouteConfigAws),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckHvnRouteExists(resourceName),
-					resource.TestCheckResourceAttr(resourceName, "hvn_route_id", hvnRouteUniqueAWSName),
+					resource.TestCheckResourceAttr(resourceName, "hvn_route_id", hvnRouteUniqueName),
 					resource.TestCheckResourceAttr(resourceName, "destination_cidr", "172.31.0.0/16"),
 					resource.TestCheckResourceAttr(resourceName, "state", "ACTIVE"),
-					testLink(resourceName, "self_link", hvnRouteUniqueAWSName, HVNRouteResourceType, "hcp_hvn.test"),
-					testLink(resourceName, "target_link", hvnRouteUniqueAWSName, PeeringResourceType, "hcp_hvn.test"),
+					testLink(resourceName, "self_link", hvnRouteUniqueName, HVNRouteResourceType, "hcp_hvn.test"),
+					testLink(resourceName, "target_link", hvnRouteUniqueName, PeeringResourceType, "hcp_hvn.test"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccHvnRouteAzure(t *testing.T) {
+	resourceName := "hcp_hvn_route.route"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t, map[string]bool{"aws": false, "azure": true}) },
+		ProtoV6ProviderFactories: testProtoV6ProviderFactories,
+		ExternalProviders: map[string]resource.ExternalProvider{
+			"azurerm": {VersionConstraint: "~> 3.63"},
+			"azuread": {VersionConstraint: "~> 2.39"},
+		},
+		CheckDestroy: testAccCheckHvnRouteDestroy,
+
+		Steps: []resource.TestStep{
+			// Testing that initial Apply created correct HVN route
+			{
+				Config: testConfig(testAccHvnRouteConfigAzure),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckHvnRouteExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "hvn_route_id", hvnRouteUniqueName),
+					resource.TestCheckResourceAttr(resourceName, "destination_cidr", "172.31.0.0/16"),
+					resource.TestCheckResourceAttr(resourceName, "state", "ACTIVE"),
+					testLink(resourceName, "self_link", hvnRouteUniqueName, HVNRouteResourceType, "hcp_hvn.test"),
+					testLink(resourceName, "target_link", hvnRouteUniqueName, PeeringResourceType, "hcp_hvn.test"),
+				),
+			},
+			// Testing that we can import HVN route created in the previous step and that the
+			// resource terraform state will be exactly the same
+			{
+				ResourceName: resourceName,
+				ImportState:  true,
+				ImportStateIdFunc: func(s *terraform.State) (string, error) {
+					rs, ok := s.RootModule().Resources[resourceName]
+					if !ok {
+						return "", fmt.Errorf("not found: %s", resourceName)
+					}
+
+					hvnID := s.RootModule().Resources["hcp_hvn.test"].Primary.Attributes["hvn_id"]
+					routeID := rs.Primary.Attributes["hvn_route_id"]
+					return fmt.Sprintf("%s:%s", hvnID, routeID), nil
+				},
+				ImportStateVerify: true,
+			},
+			// Testing running Terraform Apply for already known resource
+			{
+				Config: testConfig(testAccHvnRouteConfigAzure),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckHvnRouteExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "hvn_route_id", hvnRouteUniqueName),
+					resource.TestCheckResourceAttr(resourceName, "destination_cidr", "172.31.0.0/16"),
+					resource.TestCheckResourceAttr(resourceName, "state", "ACTIVE"),
+					testLink(resourceName, "self_link", hvnRouteUniqueName, HVNRouteResourceType, "hcp_hvn.test"),
+					testLink(resourceName, "target_link", hvnRouteUniqueName, PeeringResourceType, "hcp_hvn.test"),
 				),
 			},
 		},
