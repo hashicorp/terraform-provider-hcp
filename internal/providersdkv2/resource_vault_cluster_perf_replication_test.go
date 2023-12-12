aad223f7f91f07dc154389c27d0035570eafd2c0
helenfufu <helen.fu@hashicorp.com>
2023-12-13 04:22:43 +1100

    remove paths_filter tests on secondary due to vault 1.15.4 constraint (#696)
    * remove paths_filter tests on secondary due to vault 1.15.4 constraint
    
    * changelog
    
    * Revert "changelog"
    
    This reverts commit 6130ac390901a97eaf7d53ef0e9644887b447578.
    
    ---------
    
    Co-authored-by: Jason Pilz <jasonpilz@gmail.com>

--- internal/providersdkv2/resource_vault_cluster_perf_replication_test.go
+++ internal/providersdkv2/resource_vault_cluster_perf_replication_test.go
@@ -213,7 +213,6 @@ func performanceReplicationSteps(t *testing.T, in *inputT) []resource.TestStep {
 				hvn_id       = hcp_hvn.hvn1.hvn_id
 				tier         = lower(hcp_vault_cluster.c1.tier)
 				primary_link = hcp_vault_cluster.c1.self_link
-				paths_filter = ["path/a", "path/b"]
 			}
 			`, in)),
 			Check: resource.ComposeTestCheckFunc(
@@ -224,8 +223,6 @@ func performanceReplicationSteps(t *testing.T, in *inputT) []resource.TestStep {
 				resource.TestCheckResourceAttr(secondaryVaultResourceName, "cloud_provider", in.CloudProvider), // same as primary
 				resource.TestCheckResourceAttr(secondaryVaultResourceName, "region", in.Region),                // samae as primary
 				resource.TestCheckResourceAttr(secondaryVaultResourceName, "public_endpoint", "false"),
-				resource.TestCheckResourceAttr(secondaryVaultResourceName, "paths_filter.0", "path/a"),
-				resource.TestCheckResourceAttr(secondaryVaultResourceName, "paths_filter.1", "path/b"),
 				resource.TestCheckResourceAttr(secondaryVaultResourceName, "namespace", "admin"),
 				resource.TestCheckResourceAttrSet(secondaryVaultResourceName, "vault_version"),
 				resource.TestCheckResourceAttrSet(secondaryVaultResourceName, "organization_id"),
@@ -235,48 +232,6 @@ func performanceReplicationSteps(t *testing.T, in *inputT) []resource.TestStep {
 				resource.TestCheckResourceAttrSet(secondaryVaultResourceName, "vault_private_endpoint_url"),
 				testAccCheckFullURL(secondaryVaultResourceName, "vault_private_endpoint_url", ""),
 				resource.TestCheckResourceAttrSet(secondaryVaultResourceName, "created_at"),
-			),
-		},
-		{
-			// update paths filter
-			Config: testConfig(setTestAccPerformanceReplicationE2E(t, `
-			resource "hcp_vault_cluster" "c1" {
-				cluster_id      = "{{ .VaultClusterName }}"
-				hvn_id          = hcp_hvn.hvn1.hvn_id
-				tier            = "{{ .Tier }}"
-				public_endpoint = true
-			}
-			resource "hcp_vault_cluster" "c2" {
-				cluster_id   = "{{ .Secondary.VaultClusterName }}"
-				hvn_id       = hcp_hvn.hvn1.hvn_id
-				tier         = hcp_vault_cluster.c1.tier
-				primary_link = hcp_vault_cluster.c1.self_link
-				paths_filter = ["path/a", "path/c"]
-			}
-			`, in)),
-			Check: resource.ComposeTestCheckFunc(
-				resource.TestCheckResourceAttr(secondaryVaultResourceName, "paths_filter.0", "path/a"),
-				resource.TestCheckResourceAttr(secondaryVaultResourceName, "paths_filter.1", "path/c"),
-			),
-		},
-		{
-			// delete paths filter
-			Config: testConfig(setTestAccPerformanceReplicationE2E(t, `
-			resource "hcp_vault_cluster" "c1" {
-				cluster_id      = "{{ .VaultClusterName }}"
-				hvn_id          = hcp_hvn.hvn1.hvn_id
-				tier            = "{{ .Tier }}"
-				public_endpoint = true
-			}
-			resource "hcp_vault_cluster" "c2" {
-				cluster_id   = "{{ .Secondary.VaultClusterName }}"
-				hvn_id       = hcp_hvn.hvn1.hvn_id
-				tier         = hcp_vault_cluster.c1.tier
-				primary_link = hcp_vault_cluster.c1.self_link
-			}
-			`, in)),
-			Check: resource.ComposeTestCheckFunc(
-				resource.TestCheckNoResourceAttr(secondaryVaultResourceName, "paths_filter.0"),
 			),
 		},
 		{
