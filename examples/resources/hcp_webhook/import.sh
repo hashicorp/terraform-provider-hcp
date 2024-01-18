9ce800460d47715ff951ce6946c6a337930ee174
Sylvia Moss <moss@hashicorp.com>
2024-01-18 14:23:53 +0100

    Update examples/resources/hcp_webhook/import.sh
    Co-authored-by: Alex Dadgar <alex@hashicorp.com>

--- examples/resources/hcp_webhook/import.sh
+++ examples/resources/hcp_webhook/import.sh
@@ -1,3 +1,3 @@
 # HCP Webhook can be imported by specifying the webhook resource name
-terraform import hcp_webhook.example webhook/prokect/840e3701-55b6-4f86-8c17-b1fe397303c5/geo/us/webhook/example-webhook
+terraform import hcp_webhook.example webhook/project/840e3701-55b6-4f86-8c17-b1fe397303c5/geo/us/webhook/example-webhook
 
