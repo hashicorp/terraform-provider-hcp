737466514cf6ca1406a6b53607fdd0f963dc7726
Tobias Meyer <tobias@hashicorp.com>
2024-02-14 18:21:44 +0100

    merge with unique name change

--- internal/providersdkv2/unique_name.go
+++ internal/providersdkv2/unique_name.go
@@ -7,7 +7,7 @@ import (
 )
 
 // uniqueName will generate a unique name that is <= 36 characters long.
-// E.g. hcp-provider-test-20060102150405-123
+// E.g. hcp-provider-20060102150405-1234567
 func uniqueName() string {
-	return "hcp-provider-test-" + time.Now().Format("20060102150405") + "-" + strconv.Itoa(rand.Intn(1000))
+	return "hcp-provider-" + time.Now().Format("20060102150405") + "-" + strconv.Itoa(rand.Intn(10000000))
 }
