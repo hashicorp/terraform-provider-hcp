f765f24ba68ce6a2ccc13b803414d5bad1fa14c7
delores-hashicorp <delores.tochukwudiei@hashicorp.com>
2024-04-19 13:04:27 +0200

    Update docs to use computed resource name

--- examples/resources/hcp_vault_secrets_app_iam_binding/resource.tf
+++ examples/resources/hcp_vault_secrets_app_iam_binding/resource.tf
@@ -8,7 +8,7 @@ resource "hcp_vault_secrets_app" "example" {
 }
 
 resource "hcp_vault_secrets_app_iam_binding" "example" {
-  resource_name = "secrets/project/41d107a7-eea6-4b5e-8481-508ab29e2b07/app/example-app-name"
+  resource_name = hcp_vault_secrets_app.example.resource_name
   principal_id  = hcp_service_principal.sp.resource_id
-  role          = "roles/viewer"
+  role          = "roles/secrets.app-secret-reader"
 }
--- examples/resources/hcp_vault_secrets_app_iam_policy/resource.tf
+++ examples/resources/hcp_vault_secrets_app_iam_policy/resource.tf
@@ -1,7 +1,7 @@
 data "hcp_iam_policy" "example" {
   bindings = [
     {
-      role = "roles/contributor"
+      role = "roles/secrets.app-secret-reader"
       principals = [
         "example-user-id-1",
         "example-group-id-1",
@@ -18,6 +18,6 @@ resource "hcp_vault_secrets_app" "example" {
 }
 
 resource "hcp_vault_secrets_app_iam_policy" "example" {
-  resource_name = "secrets/project/41d107a7-eea6-4b5e-8481-508ab29e2b07/app/example-app-name"
+  resource_name = hcp_vault_secrets_app.example.resource_name
   policy_data   = data.hcp_iam_policy.example.policy_data
 }
