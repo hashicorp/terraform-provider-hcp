4cc564c0709b29040dcaf1557f95966c5beb6e3b
delores-hashicorp <delores.tochukwudiei@hashicorp.com>
2024-04-22 15:51:22 +0200

    Fix HVS ignoring default project change. This change also includes making project_id an optional field in the secrets and sercrets app schemas to allow users choose what project the app or secret should be created in.

--- internal/provider/vaultsecrets/resource_vault_secrets_app_test.go
+++ internal/provider/vaultsecrets/resource_vault_secrets_app_test.go
@@ -5,6 +5,7 @@ package vaultsecrets_test
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -13,9 +14,23 @@ import (
 
 func TestAccVaultSecretsResourceApp(t *testing.T) {
 	testAppName := generateRandomSlug()
+	projectID := os.Getenv("HCP_PROJECT_ID")
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+					resource "hcp_vault_secrets_app" "example" {
+						app_name = %q
+						description = "Acceptance test run"
+						project_id = %q
+				  }`, testAppName, projectID),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("hcp_vault_secrets_app.example", "app_name", testAppName),
+					resource.TestCheckResourceAttr("hcp_vault_secrets_app.example", "description", "Acceptance test run"),
+					resource.TestCheckResourceAttr("hcp_vault_secrets_app.example", "project_id", projectID),
+				),
+			},
 			{
 				Config: fmt.Sprintf(`
 					resource "hcp_vault_secrets_app" "example" {
--- internal/provider/vaultsecrets/resource_vault_secrets_secret_test.go
+++ internal/provider/vaultsecrets/resource_vault_secrets_secret_test.go
@@ -5,6 +5,7 @@ package vaultsecrets_test
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -12,24 +13,44 @@ import (
 )
 
 func TestAccVaultSecretsResourceSecret(t *testing.T) {
-	testAppName := generateRandomSlug()
+	testAppName1 := generateRandomSlug()
+	testAppName2 := generateRandomSlug()
+	projectID := os.Getenv("HCP_PROJECT_ID")
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
+
 			{
 				PreConfig: func() {
-					createTestApp(t, testAppName)
+					createTestApp(t, testAppName1)
 				},
 				Config: fmt.Sprintf(`
 				resource "hcp_vault_secrets_secret" "example" {
 					app_name = %q
 					secret_name = "test_secret"
 					secret_value = "super secret"
-				}`, testAppName),
+				}`, testAppName1),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("hcp_vault_secrets_secret.example", "app_name", testAppName),
+					resource.TestCheckResourceAttr("hcp_vault_secrets_secret.example", "app_name", testAppName1),
 					resource.TestCheckResourceAttr("hcp_vault_secrets_secret.example", "secret_name", "test_secret"),
 					resource.TestCheckResourceAttr("hcp_vault_secrets_secret.example", "secret_value", "super secret"),
+				),
+			},
+			{
+				PreConfig: func() {
+					createTestApp(t, testAppName2)
+				},
+				Config: fmt.Sprintf(`
+				resource "hcp_vault_secrets_secret" "example" {
+					app_name = %q
+					secret_name = "test_secret"
+					secret_value = "super secret"
+				}`, testAppName2),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("hcp_vault_secrets_secret.example", "app_name", testAppName2),
+					resource.TestCheckResourceAttr("hcp_vault_secrets_secret.example", "secret_name", "test_secret"),
+					resource.TestCheckResourceAttr("hcp_vault_secrets_secret.example", "secret_value", "super secret"),
+					resource.TestCheckResourceAttr("hcp_vault_secrets_secret.example", "project_id", projectID),
 				),
 			},
 		},
