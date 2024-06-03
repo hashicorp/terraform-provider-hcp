f7c7cfd617f15a25d0583432db1efdde408c9f10
Daniel Huckins <dhuckins@users.noreply.github.com>
2024-06-03 11:20:00 -0400

    remove tests for now

--- internal/provider/vaultsecrets/data_source_vault_secrets_rotating_secret_test.go
+++ internal/provider/vaultsecrets/data_source_vault_secrets_rotating_secret_test.go
@@ -2,32 +2,9 @@ package vaultsecrets_test
 
 import (
 	"testing"
-
-	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-
-	"github.com/hashicorp/terraform-provider-hcp/internal/provider/acctest"
 )
 
 func TestDataSourceVaultSecretsRotatingSecret(t *testing.T) {
+	// TODO
 	t.Skip("skipping the test for now, we need to be able to create a rotating secret")
-	testAppName := generateRandomSlug()
-	//dataSourceAddress := "data.hcp_vault_secrets_rotating_secret.foo"
-
-	testSecretName := "my_test_secret"
-	testSecretValue := "insecurepassword"
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { acctest.PreCheck(t) },
-		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
-		Steps: []resource.TestStep{
-			{
-				PreConfig: func() {
-					createTestApp(t, testAppName)
-
-					createTestAppSecret(t, testAppName, testSecretName, "this shouldn't show up!")
-					createTestAppSecret(t, testAppName, testSecretName, testSecretValue)
-				},
-			},
-		},
-	})
 }
