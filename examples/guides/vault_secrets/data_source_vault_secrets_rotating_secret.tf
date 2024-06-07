20f39677debe448709469d9f587aa67012f347f0
Anton Averchenkov <84287187+averche@users.noreply.github.com>
2024-06-07 17:55:19 -0400

    Update examples/guides/vault_secrets/data_source_vault_secrets_rotating_secret.tf
    Co-authored-by: Daniel Huckins <dhuckins@users.noreply.github.com>

--- examples/guides/vault_secrets/data_source_vault_secrets_rotating_secret.tf
+++ examples/guides/vault_secrets/data_source_vault_secrets_rotating_secret.tf
@@ -1,10 +1,10 @@
-data "hcp_vault_secrets_rotating_secret" "secret-data" {
+data "hcp_vault_secrets_rotating_secret" "secret_data" {
   app_name    = "example-vault-secrets-app"
   secret_name = "my_secret"
 }
 
 resource "example_resource" "example" {
-  example_attr1 = data.hcp_vault_secrets_rotating_secret.secret_values["username"]
-  example_attr2 = data.hcp_vault_secrets_rotating_secret.secret_values["password"]
+  example_attr1 = data.hcp_vault_secrets_rotating_secret.secret_data.secret_values["username"]
+  example_attr2 = data.hcp_vault_secrets_rotating_secret.secret_data.secret_values["password"]
 }
 
