b85300f6cb0bd3fc5dd1582561ef9eef2674ac01
Anton Averchenkov <84287187+averche@users.noreply.github.com>
2024-06-07 17:55:26 -0400

    Update examples/guides/vault_secrets/data_source_vault_secrets_app.tf
    Co-authored-by: Daniel Huckins <dhuckins@users.noreply.github.com>

--- examples/guides/vault_secrets/data_source_vault_secrets_app.tf
+++ examples/guides/vault_secrets/data_source_vault_secrets_app.tf
@@ -1,8 +1,8 @@
-data "hcp_vault_secrets_app" "app-data" {
+data "hcp_vault_secrets_app" "my_app" {
   app_name = "example-vault-secrets-app"
 }
 
 resource "example_resource" "example" {
-  example_attr = data.hcp_vault_secrets_secret.secrets["my_secret_key"]
+  example_attr = data.hcp_vault_secrets_app.my_app.secrets["my_secret_key"]
 }
 
