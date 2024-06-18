43432a4401e3d7b693dd8f701c6cedbb43c1f6ff
Jasper Milan <jasper.milan@hashicorp.com>
2024-06-18 10:40:23 -0400

    update tests

--- internal/provider/iam/resource_group_iam_policy_test.go
+++ internal/provider/iam/resource_group_iam_policy_test.go
@@ -32,7 +32,7 @@ func TestAccGroupIamPolicyResource(t *testing.T) {
 			{
 				Config: testAccGroupIamPolicy(resourceName, principalID, roleName),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet("hcp_group_iam_policy.example", "resource_name"),
+					resource.TestCheckResourceAttrSet("hcp_group_iam_policy.example", "name"),
 					resource.TestCheckResourceAttrSet("hcp_group_iam_policy.example", "etag"),
 					resource.TestCheckResourceAttrSet("hcp_group_iam_policy.example", "policy_data"),
 					resource.TestCheckResourceAttrPair("hcp_group_iam_policy.example", "policy_data", "data.hcp_iam_policy.example", "policy_data"),
@@ -41,14 +41,14 @@ func TestAccGroupIamPolicyResource(t *testing.T) {
 			{
 				ResourceName:                         "hcp_group_iam_policy.example",
 				ImportState:                          true,
-				ImportStateVerifyIdentifierAttribute: "resource_name",
+				ImportStateVerifyIdentifierAttribute: "name",
 				ImportStateId:                        resourceName,
 				ImportStateVerify:                    true,
 			},
 			{
 				Config: testAccGroupIamPolicy(resourceName, principalID, roleName),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet("hcp_group_iam_policy.example", "resource_name"),
+					resource.TestCheckResourceAttrSet("hcp_group_iam_policy.example", "name"),
 					resource.TestCheckResourceAttrSet("hcp_group_iam_policy.example", "etag"),
 					resource.TestCheckResourceAttrSet("hcp_group_iam_policy.example", "policy_data"),
 					resource.TestCheckResourceAttrPair("hcp_group_iam_policy.example", "policy_data", "data.hcp_iam_policy.example", "policy_data"),
@@ -77,7 +77,7 @@ func TestAccGroupIamBindingResource(t *testing.T) {
 			{
 				Config: testAccGroupIamBinding(resourceNameSuffix, principalID, roleName),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet("hcp_group_iam_binding.example", "resource_name"),
+					resource.TestCheckResourceAttrSet("hcp_group_iam_binding.example", "name"),
 					resource.TestCheckResourceAttrSet("hcp_group_iam_binding.example", "principal_id"),
 					resource.TestCheckResourceAttrSet("hcp_group_iam_binding.example", "role"),
 				),
@@ -85,7 +85,7 @@ func TestAccGroupIamBindingResource(t *testing.T) {
 			{
 				Config: testAccGroupIamBinding(resourceName, principalID, roleName),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet("hcp_group_iam_binding.example", "resource_name"),
+					resource.TestCheckResourceAttrSet("hcp_group_iam_binding.example", "name"),
 					resource.TestCheckResourceAttrSet("hcp_group_iam_binding.example", "principal_id"),
 					resource.TestCheckResourceAttrSet("hcp_group_iam_binding.example", "role"),
 				),
@@ -116,7 +116,7 @@ data "hcp_iam_policy" "example" {
 }
 
 resource "hcp_group_iam_policy" "example" {
-	resource_name = data.hcp_group.example.resource_name
+	name = data.hcp_group.example.resource_name
 	policy_data = data.hcp_iam_policy.example.policy_data
 }
 `, resourceName, principalID, roleName)
@@ -133,7 +133,7 @@ data "hcp_user_principal" "example" {
 }
 
 resource "hcp_group_iam_binding" "example" {
-	resource_name = data.hcp_group.example.resource_name
+	name = data.hcp_group.example.resource_name
 	principal_id = data.hcp_user_principal.example.user_id
 	role = %q
 }
