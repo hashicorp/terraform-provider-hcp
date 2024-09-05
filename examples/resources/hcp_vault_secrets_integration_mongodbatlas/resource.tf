e22fd3a7efc1d447a42037388d416ead52008cfe
maxcoulombe <max.coulombe@hashicorp.com>
2024-09-05 16:16:45 -0400

    * fmt

--- examples/resources/hcp_vault_secrets_integration_mongodbatlas/resource.tf
+++ examples/resources/hcp_vault_secrets_integration_mongodbatlas/resource.tf
@@ -3,6 +3,6 @@ resource "hcp_vault_secrets_integration_mongodbatlas" "example" {
   capabilities = ["ROTATION"]
   static_credential_details = {
     api_private_key = "12345678-1234-1234-1234-12345678"
-    api_public_key = "abcdefgh"
+    api_public_key  = "abcdefgh"
   }
 }
