987941e9e14bea627c8ffc76542deb6b6a808a9a
Trent DiBacco <111799824+trentdibacco@users.noreply.github.com>
2024-09-24 11:54:02 -0700

    VAULT-29412: Preview of new Vault Radar resource. (#1092)
    * VAULT-29412: Preview of new Vault Radar resource.
    
    Manage a Vault Radar source of type GitHub Enterprise via the HCP Terraform Provider.

---
+++ examples/resources/hcp_vault_radar_source_github_enterprise/resource.tf
@@ -0,0 +1,11 @@
+variable "github_enterprise_token" {
+  type      = string
+  sensitive = true
+}
+
+resource "hcp_vault_radar_source_github_enterprise" "example" {
+  domain_name         = "myserver.acme.com"
+  github_organization = "my-github-org"
+  token               = var.github_enterprise_token
+  project_id          = "my-project-id"
+}
