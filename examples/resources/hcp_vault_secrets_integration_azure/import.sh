4639f6a8fbf666746feca7b6779bf11e28688d11
Murali <murali.parthasarathy@hashicorp.com>
2024-12-02 21:32:48 +0530

    addressing review comments

--- examples/resources/hcp_vault_secrets_integration_azure/import.sh
+++ examples/resources/hcp_vault_secrets_integration_azure/import.sh
@@ -1,4 +1,4 @@
 # Vault Secrets Azure Integration can be imported by specifying the name of the integration
 # Note that since the client secret is never returned on the Vault Secrets API,
 # the next plan or apply will show a diff for that field.
-terraform import hcp_vault_secrets_integration_confluent.example my-azure-1
+terraform import hcp_vault_secrets_integration_azure.example my-azure-1
--- examples/resources/hcp_vault_secrets_integration_azure/resource.tf
+++ examples/resources/hcp_vault_secrets_integration_azure/resource.tf
@@ -2,8 +2,8 @@ resource "hcp_vault_secrets_integration_azure" "example" {
   name         = "my-azure-1"
   capabilities = ["ROTATION"]
   client_secret = {
-    "tenant_id" : "7eb3...",
-    "client_id" : "9de0...",
-    "client_secret" : "WZk8..."
+    "tenant_id" = "7eb3...",
+    "client_id" = "9de0...",
+    "client_secret" = "WZk8..."
   }
 }
