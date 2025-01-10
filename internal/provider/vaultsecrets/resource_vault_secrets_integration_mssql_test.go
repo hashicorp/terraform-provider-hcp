2ef8c6c7358addaee58501ab787762feae9cdf4e
Nagateja2402 <Nagateja590@gmail,com>
2025-01-10 11:48:50 +0530

    Added the integration for MsSQL provider

---
+++ internal/provider/vaultsecrets/resource_vault_secrets_integration_mssql_test.go
@@ -0,0 +1,154 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package vaultsecrets_test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/hcp-sdk-go/clients/cloud-vault-secrets/stable/2023-11-28/client/secret_service"
+	secretmodels "github.com/hashicorp/hcp-sdk-go/clients/cloud-vault-secrets/stable/2023-11-28/models"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+
+	"github.com/hashicorp/terraform-provider-hcp/internal/clients"
+	"github.com/hashicorp/terraform-provider-hcp/internal/provider/acctest"
+)
+
+func TestAccVaultSecretsResourceIntegrationMssql(t *testing.T) {
+	connectionString := checkRequiredEnvVarOrFail(t, "MSSQL_CONNECTION_STRING")
+
+	integrationName1 := generateRandomSlug()
+	integrationName2 := generateRandomSlug()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create initial integration with access keys
+			{
+				Config: mssqlConfig(integrationName1, connectionString),
+				Check: resource.ComposeTestCheckFunc(
+					mssqlCheckFuncs(integrationName1, connectionString)...,
+				),
+			},
+			// Changing the name forces a recreation
+			{
+				Config: mssqlConfig(integrationName2, connectionString),
+				Check: resource.ComposeTestCheckFunc(
+					mssqlCheckFuncs(integrationName2, connectionString)...,
+				),
+			},
+			// Modifying mutable fields causes an update
+			{
+				Config: mssqlConfig(integrationName2, connectionString),
+				Check: resource.ComposeTestCheckFunc(
+					mssqlCheckFuncs(integrationName2, connectionString)...,
+				),
+			},
+			// Deleting the integration out of band causes a recreation
+			{
+				PreConfig: func() {
+					t.Helper()
+					client := acctest.HCPClients(t)
+					_, err := client.VaultSecrets.DeleteIntegration(&secret_service.DeleteIntegrationParams{
+						Name:           integrationName2,
+						OrganizationID: client.Config.OrganizationID,
+						ProjectID:      client.Config.ProjectID,
+					}, nil)
+					if err != nil {
+						t.Fatal(err)
+					}
+				},
+				Config: mssqlConfig(integrationName2, connectionString),
+				Check: resource.ComposeTestCheckFunc(
+					mssqlCheckFuncs(integrationName2, connectionString)...,
+				),
+				PlanOnly:           true,
+				ExpectNonEmptyPlan: true,
+			},
+			// Pre-existing integration can be imported
+			{
+				PreConfig: func() {
+					t.Helper()
+					client := acctest.HCPClients(t)
+					_, err := client.VaultSecrets.CreateIntegration(&secret_service.CreateIntegrationParams{
+						Body: &secretmodels.SecretServiceCreateIntegrationBody{
+							Capabilities: []*secretmodels.Secrets20231128Capability{secretmodels.Secrets20231128CapabilityROTATION.Pointer()},
+							MssqlStaticCredentials: &secretmodels.Secrets20231128MssqlStaticCredentialsRequest{
+								ConnectionString: connectionString,
+							},
+							Name: integrationName2,
+						},
+						OrganizationID: client.Config.OrganizationID,
+						ProjectID:      client.Config.ProjectID,
+					}, nil)
+					if err != nil {
+						t.Fatal(err)
+					}
+				},
+				Config: mssqlConfig(integrationName2, connectionString),
+				Check: resource.ComposeTestCheckFunc(
+					mssqlCheckFuncs(integrationName2, connectionString)...,
+				),
+				ResourceName:  "hcp_vault_secrets_integration.acc_test",
+				ImportStateId: integrationName2,
+				ImportState:   true,
+			},
+		},
+		CheckDestroy: func(_ *terraform.State) error {
+			if mssqlIntegrationExists(t, integrationName1) {
+				return fmt.Errorf("test mssql integration %s was not destroyed", integrationName1)
+			}
+			if mssqlIntegrationExists(t, integrationName2) {
+				return fmt.Errorf("test mssql integration %s was not destroyed", integrationName2)
+			}
+			return nil
+		},
+	})
+}
+
+func mssqlConfig(integrationName, connectionString string) string {
+	return fmt.Sprintf(`
+	resource "hcp_vault_secrets_integration" "acc_test" {
+		name = %q
+		capabilities = ["ROTATION"]
+        provider_type = "mssql"
+		mssql_static_credentials = {
+			connection_string = %q
+	   }
+    }`, integrationName, connectionString)
+}
+
+func mssqlCheckFuncs(integrationName, connectionString string) []resource.TestCheckFunc {
+	return []resource.TestCheckFunc{
+		resource.TestCheckResourceAttrSet("hcp_vault_secrets_integration.acc_test", "organization_id"),
+		resource.TestCheckResourceAttr("hcp_vault_secrets_integration.acc_test", "project_id", os.Getenv("HCP_PROJECT_ID")),
+		resource.TestCheckResourceAttrSet("hcp_vault_secrets_integration.acc_test", "resource_id"),
+		resource.TestCheckResourceAttrSet("hcp_vault_secrets_integration.acc_test", "resource_name"),
+		resource.TestCheckResourceAttr("hcp_vault_secrets_integration.acc_test", "name", integrationName),
+		resource.TestCheckResourceAttr("hcp_vault_secrets_integration.acc_test", "capabilities.#", "1"),
+		resource.TestCheckResourceAttr("hcp_vault_secrets_integration.acc_test", "capabilities.0", "ROTATION"),
+		resource.TestCheckResourceAttr("hcp_vault_secrets_integration.acc_test", "provider_type", "mssql"),
+		resource.TestCheckResourceAttr("hcp_vault_secrets_integration.acc_test", "mssql_static_credentials.connection_string", connectionString),
+	}
+}
+
+func mssqlIntegrationExists(t *testing.T, name string) bool {
+	t.Helper()
+
+	client := acctest.HCPClients(t)
+
+	response, err := client.VaultSecrets.GetIntegration(
+		secret_service.NewGetIntegrationParamsWithContext(ctx).
+			WithOrganizationID(client.Config.OrganizationID).
+			WithProjectID(client.Config.ProjectID).
+			WithName(name), nil)
+	if err != nil && !clients.IsResponseCodeNotFound(err) {
+		t.Fatal(err)
+	}
+
+	return !clients.IsResponseCodeNotFound(err) && response != nil && response.Payload != nil && response.Payload.Integration != nil
+}
