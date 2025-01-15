f5fadde50f06f75d1da9e191529356702efaff8c
Tushar Chaudhary <tushar.chaudhary@tushar.chaudhary-LFG2WD0J59>
2025-01-15 12:34:58 +0530

    Create Rotating Secret MySQL

--- internal/provider/vaultsecrets/resource_vault_secrets_rotating_secret.go
+++ internal/provider/vaultsecrets/resource_vault_secrets_rotating_secret.go
@@ -72,6 +72,7 @@ type RotatingSecret struct {
 	TwilioAPIKey             *twilioAPIKey             `tfsdk:"twilio_api_key"`
 	ConfluentServiceAccount  *confluentServiceAccount  `tfsdk:"confluent_service_account"`
 	AzureApplicationPassword *AzureApplicationPassword `tfsdk:"azure_application_password"`
+	MysqlUsers               *mysqlUsers               `tfsdk:"mysql_users"`
 	// Computed fields
 	OrganizationID types.String `tfsdk:"organization_id"`
 
@@ -103,6 +104,10 @@ type AzureApplicationPassword struct {
 }
 
 type twilioAPIKey struct{}
+
+type mysqlUsers struct {
+	Usernames []string `json:"usernames"`
+}
 
 var _ resource.Resource = &resourceVaultSecretsRotatingSecret{}
 var _ resource.ResourceWithConfigure = &resourceVaultSecretsRotatingSecret{}
