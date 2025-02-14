b2f58bcec5e80cf732e746a6eacbadb93f03ff13
Jeff Campbell <jeff.campbell@jeff.campbell-JRQCQ00G0G>
2025-02-14 11:10:11 -0500

    adding proper component IDs

--- internal/provider/statuspage.go
+++ internal/provider/statuspage.go
@@ -4,98 +4,101 @@
 package provider
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
+        "encoding/json"
+        "fmt"
+        "io"
+        "net/http"
+        "time"
 
-	diagnostic "github.com/hashicorp/terraform-plugin-framework/diag"
+        diagnostic "github.com/hashicorp/terraform-plugin-framework/diag"
 )
 
 // Status endpoint for prod.
-const statuspageURL = "https://status.hashicorp.com/api/v2/components.json"
+const statuspageURL = "https://status.hashicorp.com/api/v1/summary"
 
-var hcpComponentIds = map[string]string{
-	"0q55nwmxngkc": "HCP API",
-	"sxffkgfb4fhb": "HCP Consul",
-	"0mbkqnrzg33w": "HCP Packer",
-	"mgv1p2j9x444": "HCP Portal",
-	"mb7xrbx9gjnq": "HCP Vault",
-	"9gzfhnxdsf93": "HCP Boundary",
+var hcpComponentNames = map[string]string{
+        "HCP API":      "01JK8R0BRY4185T4NHJFAXP35D",
+        "HCP Consul":   "01JKJP1CYEVMQKJ1Z5WAX0W12F", // Mapped to the HCP Consul Dedicated group
+        "HCP Packer":   "01JK8R0BRYR9EYAGMNJ5EKC6CS",
+        "HCP Portal":   "01JK8R0BRYKPJS5K35R2ZCSHV0",
+        "HCP Vault":    "01JKJP1CYEVMQKJ1Z5WESBEQ7M", // Mapped to the HCP Vault Dedicated group
+        "HCP Boundary": "01JK8R0BRYHN4JYQ1H3WC42RWV",
 }
 
 type statuspage struct {
-	Components []component `json:"components"`
+        OngoingIncidents []incident `json:"ongoing_incidents"`
 }
 
-type component struct {
-	ID     string `json:"id"`
-	Status status `json:"status"`
+type incident struct {
+        Name             string          `json:"name"`
+        Status           string          `json:"status"`
+        CurrentWorstImpact string          `json:"current_worst_impact"`
+        AffectedComponents []affectedComponent `json:"affected_components"`
+        LastUpdateAt     string          `json:"last_update_at"`
+        LastUpdateMessage string          `json:"last_update_message"`
 }
 
-type status string
+type affectedComponent struct {
+        ID        string `json:"id"`
+        Name      string `json:"name"`
+        GroupName string `json:"group_name,omitempty"`
+}
 
-// This functionality is a duplicate of isHCPOperational in order to return
-// the proper diagnostics for the terraform plugin framework
 func isHCPOperationalFramework() (diags diagnostic.Diagnostics) {
-	req, err := http.NewRequest("GET", statuspageURL, nil)
-	if err != nil {
-		diags.AddWarning("You may experience issues using HCP.",
-			fmt.Sprintf("Unable to create request to verify HCP status: %s", err))
+        req, err := http.NewRequest("GET", statuspageURL, nil)
+        if err != nil {
+                diags.AddWarning("You may experience issues using HCP.",
+                        fmt.Sprintf("Unable to create request to verify HCP status: %s", err))
+                return diags
+        }
 
-		return diags
-	}
+        var cl = http.Client{}
+        resp, err := cl.Do(req)
+        if err != nil {
+                diags.AddWarning("You may experience issues using HCP.",
+                        fmt.Sprintf("Unable to create request to verify HCP status: %s", err))
+                return diags
+        }
+        defer resp.Body.Close()
 
-	var cl = http.Client{}
-	resp, err := cl.Do(req)
-	if err != nil {
-		diags.AddWarning("You may experience issues using HCP.",
-			fmt.Sprintf("Unable to create request to verify HCP status: %s", err))
+        jsBytes, err := io.ReadAll(resp.Body)
+        if err != nil {
+                diags.AddWarning("You may experience issues using HCP.",
+                        fmt.Sprintf("Unable read response to verify HCP status: %s", err))
+                return diags
+        }
 
-		return diags
-	}
-	defer resp.Body.Close()
+    	sp := statuspage{}
+    	err = json.Unmarshal(jsBytes, &sp)
+        if err != nil {
+                diags.AddWarning("You may experience issues using HCP.",
+                        fmt.Sprintf("Unable unmarshal response to verify HCP status: %s", err))
+                return diags
+        }
+	
+    	systemStatus := make(map[string]string)
+    	operational := true
+    	statusMessage := ""
+	
+    	for _, inc := range sp.OngoingIncidents {
+    	    for _, comp := range inc.AffectedComponents {
+    	        if expectedID, ok := hcpComponentNames[comp.Name]; ok && expectedID == comp.ID {
+    	            impact := inc.CurrentWorstImpact
+    	            systemStatus[comp.Name] = impact
+    	            if impact != "operational" {
+    	                operational = false
+    	                statusMessage += fmt.Sprintf("%s: %s (%s - %s)\n", comp.Name, impact, inc.Name, inc.LastUpdateMessage)
+    	            }
+    	        }
+    	    }
+    	}
+	
+    	if !operational {
+    	    diags.AddWarning("You may experience issues using HCP.",
+    	        fmt.Sprintf("HCP is reporting the following:\n\n%s\nPlease check https://status.hashicorp.com for more details.", statusMessage))
+    	}
 
-	jsBytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		diags.AddWarning("You may experience issues using HCP.",
-			fmt.Sprintf("Unable read response to verify HCP status: %s", err))
-
-		return diags
-	}
-
-	sp := statuspage{}
-	err = json.Unmarshal(jsBytes, &sp)
-	if err != nil {
-		diags.AddWarning("You may experience issues using HCP.",
-			fmt.Sprintf("Unable unmarshal response to verify HCP status: %s", err))
-
-		return diags
-	}
-
-	// Translate the status page component IDs into a map of component name and operation status.
-	var systemStatus = map[string]status{}
-
-	for _, c := range sp.Components {
-		name, ok := hcpComponentIds[c.ID]
-		if ok {
-			systemStatus[name] = c.Status
-		}
-	}
-
-	operational := true
-	for _, st := range systemStatus {
-		if st != "operational" {
-			operational = false
-		}
-	}
-
-	if !operational {
-		diags.AddWarning("You may experience issues using HCP.",
-			fmt.Sprintf("HCP is reporting the following:\n\n%v\nPlease check https://status.hashicorp.com for more details.", printStatus(systemStatus)))
-	}
-
-	return diags
+    return diags
 }
 
 func printStatus(m map[string]status) string {
