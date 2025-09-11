6c5776514d5a541df4de466da96fa7f61274250e
aahel <aahel.guha@hashicorp.com>
2025-09-11 15:16:23 +0530

    fix fmt

--- examples/resources/hcp_private_link/resource.tf
+++ examples/resources/hcp_private_link/resource.tf
@@ -16,7 +16,7 @@ resource "hcp_vault_cluster" "example" {
   hvn_id          = hcp_hvn.example.hvn_id
   tier            = "standard_small"
   public_endpoint = false
-  
+
   major_version_upgrade_config {
     upgrade_type = "AUTOMATIC"
   }
@@ -27,12 +27,12 @@ resource "hcp_private_link" "example" {
   hvn_id           = hcp_hvn.example.hvn_id
   private_link_id  = "example-private-link"
   vault_cluster_id = hcp_vault_cluster.example.cluster_id
-  
+
   # AWS account IDs allowed to connect to this private link
   consumer_accounts = [
     "arn:aws:iam:123456789012:root"
   ]
-  
+
   # AWS regions from which you can connect to this private link
   consumer_regions = [
     "us-west-2",
